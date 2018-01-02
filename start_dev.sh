9ec82b132106cc396183267d529b4ee44d907797
LiQiang <460810619@qq.com>
2018-01-02 17:50:21 +0800

    up

--- start_dev.sh
+++ start_dev.sh
@@ -1,8 +1,7 @@
 #! /bin/sh 
 basepath=$(cd `dirname $0`; pwd)
 pids_path=${basepath}/tmp/pids/
-echo $pids_path
-if [ ! -d "$pids_path"]; then  
+if [ ! -d "$pids_path"]; then 
 　　mkdir "$pids_path"  
 fi  
 bundle exec puma -C ./config/puma.rb -e development
