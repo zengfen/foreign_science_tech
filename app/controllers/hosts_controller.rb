c4053525840a22c0c1a442da681bce58ec1a0af7
yqingp <yqingp@gmail.com>
2018-01-03 15:40:13 +0800

    1

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,6 +1,26 @@
 class HostsController < ApplicationController
 
   def index
+    hosts = $archon_redis.hgetall("host_list")
+    agents_status = $archon_redis.hgetall("host_agent_status_list")
+
+
+    @hosts = []
+
+    hosts.each do |k, v|
+      agent_status = agents_status[k]
+      c = GeoIP.new('GeoIP.dat').country(k)
+      country = c.country_code == "CN" ? "国内" : "国外"
+
+      if agent_status.blank?
+        @hosts << [k, country, Time.now.to_i - v.to_i, "未知"]
+      elsif agent_status == "true"
+        @hosts << [k, country, Time.now.to_i - v.to_i, "运行中"]
+      else
+        @hosts << [k, country, Time.now.to_i - v.to_i, "停止"]
+      end
+    end
+
   end
 
 end
