c84ce039ba5023c6126f2de9dd498a26d5c6880a
yqingp <yqingp@gmail.com>
2018-01-03 15:16:00 +0800

    1

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -14,9 +14,9 @@ class CommandsController < ApplicationController
   def create
     command = params[:command]
 
-    if !$redis.exists("controller_command")
-      $redis.set("controller_command", {"command" => command, "command_magic" => Time.now.to_i.to_s }.to_json)
-      $redis.expire("controller_command", 60) # 1分钟过期
+    if !$archon_redis.exists("controller_command")
+      $archon_redis.set("controller_command", {"command" => command, "command_magic" => Time.now.to_i.to_s }.to_json)
+      $archon_redis.expire("controller_command", 60) # 1分钟过期
     end
 
     redirect_to action: :index
