aa15d4deda5b1692f8684ac152c5a837f659d96c
yqingp <yqingp@gmail.com>
2018-01-04 09:36:21 +0800

    1

--- app/controllers/host_metrics_controller.rb
+++ app/controllers/host_metrics_controller.rb
@@ -3,4 +3,10 @@ class HostMetricsController < ApplicationController
   def index
   end
 
+
+  def show
+    ip = params[:ip]
+    @results = $archon_redis.hgetall("host_metric_#{ip}")
+  end
+
 end
