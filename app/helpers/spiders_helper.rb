51a713d5bc8944a7b5293f7d44b814f2c742662f
LiQiang <460810619@qq.com>
2018-01-05 18:54:42 +0800

    up

--- app/helpers/spiders_helper.rb
+++ app/helpers/spiders_helper.rb
@@ -1,2 +1,6 @@
 module SpidersHelper
+	def format_additional_function(additional_function)
+		text = additional_function.collect{|x| x.collect{|k,v| "#{k}:#{v}"}}.join(',') rescue ""
+		return text
+	end
 end
