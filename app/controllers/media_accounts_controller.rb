a48cd74e1a4f21dba3f82f46506d8efeb43fd28e
Ray <xulei324@gmail.com>
2018-01-07 16:07:35 +0800

    1

--- app/controllers/media_accounts_controller.rb
+++ app/controllers/media_accounts_controller.rb
@@ -1,9 +1,11 @@
 class MediaAccountsController < ApplicationController
-	before_action :logged_in_user
+  before_action :logged_in_user
 
-	def index
-		opts = {}
+  def index
+    opts = {}
     opts[:name] = params[:keyword] if !params[:keyword].blank?
-		@media_accounts = MediaAccount.where(opts).order("created_at desc").page(params[:page]).per(20)
-	end
+    opts[:account_type] = params[:account_type] if !params[:account_type].blank?
+    @media_accounts = MediaAccount.where(opts).order("created_at desc").page(params[:page]).per(20)
+  end
+
 end
