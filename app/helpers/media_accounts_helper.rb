fe05ae4fc1cf92cbe26c2d9ba9375b2007b7a951
LiQiang <460810619@qq.com>
2018-01-07 15:40:34 +0800

    up

--- app/helpers/media_accounts_helper.rb
+++ app/helpers/media_accounts_helper.rb
@@ -1,2 +1,13 @@
 module MediaAccountsHelper
+def media_account_type_i(account_type)
+		i_class = ""
+		case account_type
+		when "twitter"
+			i_class = "fa-twitter"
+		when "facebook"
+			i_class = "fa-facebook"
+		end
+		html = "<i class=\"fa #{i_class}\" title=\"#{account_type}\"></i>"
+		return html
+end
 end
