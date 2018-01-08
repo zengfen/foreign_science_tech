7d6edd299173cc75836d13c9cf7525bcf2df6f40
LiQiang <460810619@qq.com>
2018-01-08 16:02:48 +0800

    up

--- app/controllers/media_accounts_controller.rb
+++ app/controllers/media_accounts_controller.rb
@@ -4,7 +4,6 @@ class MediaAccountsController < ApplicationController
   def index
     opts = {}
     opts[:name] = params[:keyword] if !params[:keyword].blank?
-    opts[:account_type] = params[:account_type] if !params[:account_type].blank?
     @media_accounts = MediaAccount.where(opts).order("created_at desc").page(params[:page]).per(20)
   end
 
--- app/models/media_account.rb
+++ app/models/media_account.rb
@@ -12,4 +12,46 @@
 #
 
 class MediaAccount < ApplicationRecord
+
+	def self.init_data(path)
+	  type = path.split(".").last
+    begin
+      case type
+      when "xlsx" then
+        ss = Roo::Excelx.new(path)
+      when "xls" then
+        ss = Roo::Excel.new(path)
+      when "csv" then
+        ss = Roo::CSV.new(path)
+      end
+    rescue Exception=>e
+      return false
+    end
+    ss.sheets.each do |s|
+      ss.default_sheet = s
+      header = ss.row(1).collect{|x| x.strip}
+      for i in (ss.first_row+1)..ss.last_row
+        row = Hash[[header, ss.row(i)].transpose]
+        name = row["名称"].to_s.strip
+        short_name = JSON.parse(row["简称"]).first
+        ma = MediaAccount.find_by(:name=>name)
+        ma = MediaAccount.new if ma.blank?
+        ma.name = name
+        ma.short_name = short_name
+        ma.status = rand(0..1)
+        ma.save
+      end
+    end 
+	end
+
+
+	def self.statuses
+		{0=>"停止",1=>"采集中"}
+	end
+
+	def status_cn
+		MediaAccount.statuses[self.status]
+	end
+
+
 end
