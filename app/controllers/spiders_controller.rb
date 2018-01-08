d11492bbcd87d3d356d772313d22f2663b8661be
LiQiang <460810619@qq.com>
2018-01-08 10:20:53 +0800

    up

--- app/controllers/spiders_controller.rb
+++ app/controllers/spiders_controller.rb
@@ -3,9 +3,8 @@ class SpidersController < ApplicationController
 	before_action :get_spider, only: [:show,:show_info, :load_edit_form, :update, :destroy]
 	
 	def index
-		params[:category]||=-1
 		opts = {}
-		opts[:category] = params[:category] if  params[:category]!=-1
+		opts[:category] = params[:category] if  params[:category].blank?
     opts[:spider_name] = params[:keyword] if !params[:keyword].blank?
 		@spiders = Spider.where(opts).order("created_at desc").page(params[:page]).per(10)
 		@spider = Spider.new
