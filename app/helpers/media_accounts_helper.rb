1775bc7bb18b887fd0f08f8a5ad76321c8156bcb
LiQiang <460810619@qq.com>
2018-01-08 10:13:39 +0800

    up

--- app/helpers/media_accounts_helper.rb
+++ app/helpers/media_accounts_helper.rb
@@ -6,6 +6,10 @@ def media_account_type_i(account_type)
 			i_class = "fa-twitter"
 		when "facebook"
 			i_class = "fa-facebook"
+		when "youtube"
+			i_class = "fa-youtube"
+		when "linkedin"
+			i_class = "fa-linkedin"
 		end
 		html = "<i class=\"fa #{i_class}\" title=\"#{account_type}\"></i>"
 		return html
--- app/models/media_account.rb
+++ app/models/media_account.rb
@@ -17,9 +17,10 @@ class MediaAccount < ApplicationRecord
 		file = File.read(path)
 		json_value = JSON.parse(file)
 		json_value.each do |k,v|
-			next if (!v.has_key?"fb")&&(!v.has_key?"tw")
+			next if (!v.has_key?"fb")&&(!v.has_key?"tw")&&(!v.has_key?"ytb")&&(!v.has_key?"lk")
 	  	if !v["fb"].blank? && v["fb"]!="未发现"
-	  		ma = MediaAccount.new
+	  	  ma = MediaAccount.find_by(account: v["fb"])
+	  		ma = MediaAccount.new if ma.blank?
 	  		ma.name = k
 	  		ma.account_type = "facebook"
 	  		ma.account = v["fb"]
@@ -27,10 +28,29 @@ class MediaAccount < ApplicationRecord
 	  		ma.save
 	  	end
 	  	if !v["tw"].blank? && v["tw"]!="未发现"
-	  		ma = MediaAccount.new
+	  	  ma = MediaAccount.find_by(account: v["tw"])
+	  		ma = MediaAccount.new if ma.blank?
 	  		ma.name = k
 	  		ma.account_type = "twitter"
 	  		ma.account = v["tw"]
+	  		ma.status = rand(0..1)
+	  		ma.save
+	  	end
+	  	if !v["ytb"].blank? && v["ytb"]!="未发现"
+	  		ma = MediaAccount.find_by(account: v["ytb"])
+	  		ma = MediaAccount.new if ma.blank?
+	  		ma.name = k
+	  		ma.account_type = "youtube"
+	  		ma.account = v["ytb"]
+	  		ma.status = rand(0..1)
+	  		ma.save
+	  	end
+	  	if !v["lk"].blank? && v["lk"]!="未发现"
+	  		ma = MediaAccount.find_by(account: v["lk"])
+	  		ma = MediaAccount.new if ma.blank?
+	  		ma.name = k
+	  		ma.account_type = "linkedin"
+	  		ma.account = v["lk"]
 	  		ma.status = rand(0..1)
 	  		ma.save
 	  	end
