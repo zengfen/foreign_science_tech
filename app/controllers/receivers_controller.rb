a89a65a29129a74c33fc58d93f50540ab21c0f51
yqingp <yqingp@gmail.com>
2018-01-18 16:17:13 +0800

    1

--- app/controllers/receivers_controller.rb
+++ app/controllers/receivers_controller.rb
@@ -1,6 +1,14 @@
 class ReceiversController < ApplicationController
 
   def index
+    @all_receivers = $redis.hgetall("archon_receivers")
+    @common_receivers = $redis.zrange("common_receivers_pool", 0, -1, withscores: true)
+    @ip_receivers = {}
+    ip_receivers_ips  = $redis.keys("ip_receivers_*")
+    ip_receivers_ips.each do |ip_key|
+      ip = ip_key.gsub("ip_receivers_", "")
+      @ip_receivers[ip] = $redis.zrange(ip_key, 0, -1, withscores: true)
+    end
   end
 
 end
