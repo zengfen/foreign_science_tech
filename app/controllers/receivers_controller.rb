129015040e8a5304e7abf324100ae3278d901feb
yqingp <yqingp@gmail.com>
2018-01-18 16:54:24 +0800

    1

--- app/controllers/receivers_controller.rb
+++ app/controllers/receivers_controller.rb
@@ -1,31 +1,27 @@
 class ReceiversController < ApplicationController
-
   def index
-    @all_receivers = $archon_redis.hgetall("archon_receivers")
-    @common_receivers = $archon_redis.zrange("common_receivers_pool", 0, -1, withscores: true)
+    @all_receivers = $archon_redis.hgetall('archon_receivers')
+    @common_receivers = $archon_redis.zrange('common_receivers_pool', 0, -1, withscores: true)
     @ip_receivers = {}
-    ip_receivers_ips  = $archon_redis.keys("ip_receivers_*")
+    ip_receivers_ips = $archon_redis.keys('ip_receivers_*')
     ip_receivers_ips.each do |ip_key|
-      ip = ip_key.gsub("ip_receivers_", "")
+      ip = ip_key.gsub('ip_receivers_', '')
       @ip_receivers[ip] = $archon_redis.zrange(ip_key, 0, -1, withscores: true)
     end
   end
 
-
   def set_common
-    $archon_redis.zadd("common_receivers_pool", 0, params[:ip])
+    $archon_redis.zadd('common_receivers_pool', 0, params[:ip])
 
-    redirect_to :back
+    redirect_back
   end
-
 
   def set_agent
     #  选择一组agent hosts
     #
-    agent_ip = ""
+    agent_ip = ''
     $archon_redis.zadd("ip_receivers_#{agent_ip}", 0, params[:ip])
 
-    redirect_to :back
+    redirect_back
   end
-
 end
