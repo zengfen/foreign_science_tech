946c759ca9a34c65dd35cfed2181bf5b21e7b7bd
yqingp <yqingp@gmail.com>
2018-01-18 17:40:41 +0800

    1

--- app/models/spider_task.rb
+++ app/models/spider_task.rb
@@ -17,7 +17,8 @@
 
 class SpiderTask < ApplicationRecord
 	validates :spider_id, presence: true
-	validates :level, numericality: { only_integer: true,greater_than_or_equal_to:1,less_than_or_equal_to:10 }
+	validates :level, numericality: { only_integer: true, greater_than_or_equal_to:1,less_than_or_equal_to:10 }
+	validates :max_retry_count, numericality: { only_integer: true, greater_than_or_equal_to:1,less_than_or_equal_to:10 }
 
 	belongs_to :spider
 
