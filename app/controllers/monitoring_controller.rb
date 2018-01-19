42a664ed72d0e0badaf7f47acd5b7e3f548554fb
Ray <xulei324@gmail.com>
2018-01-19 18:01:31 +0800

    1

--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -3,12 +3,5 @@ class MonitoringController < ApplicationController
     body = RestClient.get("http://10.27.3.39:9200/_cluster/health?pretty=true").body
     @cluster = JSON.parse(body)
     @nodes =  JSON.parse(RestClient.get("http://10.27.3.39:9200/_nodes/stats/fs,jvm,http,process?human&pretty&pretty").body)
-
-
-    opts = {}
-    opts[:category] = params[:category] if  !params[:category].blank?
-    opts[:spider_name] = params[:keyword] if !params[:keyword].blank?
-    @spiders = Spider.where(opts).order("created_at desc").page(params[:page]).per(10)
-    @spider = Spider.new
   end
 end
