442522a59841ab1dca24ad9a2aad599192bf8946
Ray <xulei324@gmail.com>
2018-01-19 19:02:39 +0800

    1

--- app/controllers/lucene_controller.rb
+++ app/controllers/lucene_controller.rb
@@ -1,7 +1,8 @@
 class LuceneController < ApplicationController
   def index
-    body = RestClient.get("http://10.27.3.39:9200/_cluster/health?pretty=true").body
-    @cluster = JSON.parse(body)
+    body = RestClient.get("http://10.27.3.39:9200/_cat/indices?format=json&pretty").body
+    indices = JSON.parse(body)
+    @indices = Kaminari.paginate_array(indices, total_count: indices.count).page(params[:page]).per(50)
     opts = {}
     opts[:category] = params[:category] if  !params[:category].blank?
     opts[:spider_name] = params[:keyword] if !params[:keyword].blank?
