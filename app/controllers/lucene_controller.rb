b07aa23eed6a37ce3eb1ae67e5fc3523909cc245
Ray <xulei324@gmail.com>
2018-01-19 16:45:41 +0800

    1

--- app/controllers/lucene_controller.rb
+++ app/controllers/lucene_controller.rb
@@ -1,5 +1,7 @@
 class LuceneController < ApplicationController
   def index
+    body = RestClient.get("http://10.27.3.39:9200/_cluster/health?pretty=true").body
+    @cluster = JSON.parse(body)
     opts = {}
     opts[:category] = params[:category] if  !params[:category].blank?
     opts[:spider_name] = params[:keyword] if !params[:keyword].blank?
