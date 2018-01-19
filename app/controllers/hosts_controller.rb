ee4292c6ad796420849f334b0dde5ef7438350e1
yqingp <yqingp@gmail.com>
2018-01-19 19:57:59 +0800

    1

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -2,19 +2,18 @@ class HostsController < ApplicationController
   include ActionView::Helpers::DateHelper
   before_action :logged_in_user
   def index
-    hosts = $archon_redis.hgetall("archon_hosts")
+    hosts = $archon_redis.hgetall('archon_hosts')
     @hosts = []
 
     hosts.each do |k, v|
       c = $geo_ip.country(k)
-      country = c.country_code2 == "CN" ? "国内" : "国外"
+      country = c.country_code2 == 'CN' ? '国内' : '国外'
       service_count = $archon_redis.hlen("archon_host_services_#{k}")
 
-      running_service_count = $archon_redis.hgetall("archon_host_services_#{k}").count{|m, n| n == "true"}
+      running_service_count = $archon_redis.hgetall("archon_host_services_#{k}").count { |_m, n| n == 'true' }
 
       @hosts << [k, country, Time.at(v.to_i), service_count, running_service_count]
     end
-
 
     # @hosts = []
 
@@ -36,7 +35,13 @@ class HostsController < ApplicationController
     #     @hosts << [k, country, Time.at(v.to_i), "停止"]
     #   end
     # end
-
   end
 
+  def show
+    @ip = params[:ip]
+    @heartbeat_at = Time.at($archon_redis.hget('archon_hosts', @ip).to_i)
+    c = $geo_ip.country(@ip)
+    @country = c.country_code2 == 'CN' ? '国内' : '国外'
+    @services = $archon_redis.hgetall("archon_host_services_#{@ip}")
+  end
 end
