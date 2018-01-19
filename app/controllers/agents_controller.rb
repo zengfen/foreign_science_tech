bc71ff48c2bc30fd6e0ea9e0e407c45df33f3844
yqingp <yqingp@gmail.com>
2018-01-19 22:09:08 +0800

    1

---
+++ app/controllers/agents_controller.rb
@@ -0,0 +1,15 @@
+class AgentsController < ApplicationController
+  def index
+    service_name = 'agent'
+    @results = {}
+    $archon_redis.keys('archon_host_services_*').each do |key|
+      status = $archon_redis.hexist(key, service_name)
+      next if status.blank?
+      ip = key.gsub('archon_host_services_', '')
+      heartbeat_at = $archon_redis.hget('archon_hosts', ip).to_i
+      c = $geo_ip.country(ip)
+      country = c.country_code2 == 'CN' ? '国内' : '国外'
+      @results << [ip, country, status, heartbeat_at]
+    end
+  end
+end
--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -12,6 +12,7 @@ class HostsController < ApplicationController
 
       running_service_count = $archon_redis.hgetall("archon_host_services_#{k}").count { |_m, n| n == 'true' }
 
+      # 心跳间隔潮所1分钟，所有的状态修改为未知
       @hosts << [k, country, Time.at(v.to_i), service_count, running_service_count]
     end
 
@@ -39,6 +40,7 @@ class HostsController < ApplicationController
 
   def show
     @ip = params[:ip]
+    # 心跳间隔潮所1分钟，所有的状态修改为未知
     @heartbeat_at = Time.at($archon_redis.hget('archon_hosts', @ip).to_i)
     c = $geo_ip.country(@ip)
     @country = c.country_code2 == 'CN' ? '国内' : '国外'
