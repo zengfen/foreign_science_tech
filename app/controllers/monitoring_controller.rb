82009ce32da2cb152550da350d50a847f20bec5b
Ray <xulei324@gmail.com>
2018-01-19 17:05:35 +0800

    1

---
+++ app/controllers/monitoring_controller.rb
@@ -0,0 +1,11 @@
+class MonitoringController < ApplicationController
+  def lucene
+    body = RestClient.get("http://10.27.3.39:9200/_cluster/health?pretty=true").body
+    @cluster = JSON.parse(body)
+    opts = {}
+    opts[:category] = params[:category] if  !params[:category].blank?
+    opts[:spider_name] = params[:keyword] if !params[:keyword].blank?
+    @spiders = Spider.where(opts).order("created_at desc").page(params[:page]).per(10)
+    @spider = Spider.new
+  end
+end
