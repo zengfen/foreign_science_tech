7897adfad76096a45be6d67bf9b5ee1437517825
yqingp <yqingp@gmail.com>
2018-01-19 22:16:25 +0800

    1

--- app/controllers/receivers_controller.rb
+++ app/controllers/receivers_controller.rb
@@ -1,13 +1,24 @@
 class ReceiversController < ApplicationController
   def index
-    @all_receivers = $archon_redis.hgetall('archon_receivers')
-    @common_receivers = $archon_redis.zrange('archon_common_receivers_pool', 0, -1, withscores: true)
-    @ip_receivers = {}
-    ip_receivers_ips = $archon_redis.keys('archon_ip_receivers_*')
-    ip_receivers_ips.each do |ip_key|
-      ip = ip_key.gsub('archon_ip_receivers_', '')
-      @ip_receivers[ip] = $archon_redis.zrange(ip_key, 0, -1, withscores: true)
+    service_name = 'receiver'
+    @agents = []
+    $archon_redis.keys('archon_host_services_*').each do |key|
+      status = $archon_redis.hget(key, service_name)
+      next if status.blank?
+      ip = key.gsub('archon_host_services_', '')
+      heartbeat_at = $archon_redis.hget('archon_hosts', ip).to_i
+      c = $geo_ip.country(ip)
+      country = c.country_code2 == 'CN' ? '国内' : '国外'
+      @agents << [ip, country,  heartbeat_at, status == "true" ? "运行中" : "已停止"]
     end
+    # @all_receivers = $archon_redis.hgetall('archon_receivers')
+    # @common_receivers = $archon_redis.zrange('archon_common_receivers_pool', 0, -1, withscores: true)
+    # @ip_receivers = {}
+    # ip_receivers_ips = $archon_redis.keys('archon_ip_receivers_*')
+    # ip_receivers_ips.each do |ip_key|
+    #   ip = ip_key.gsub('archon_ip_receivers_', '')
+    #   @ip_receivers[ip] = $archon_redis.zrange(ip_key, 0, -1, withscores: true)
+    # end
   end
 
   def set_common
