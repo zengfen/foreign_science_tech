7a1c599efeb992dda793ccc99fd26f2e490a93c8
Ray <xulei324@gmail.com>
2018-01-19 17:58:37 +0800

    1

--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -2,6 +2,9 @@ class MonitoringController < ApplicationController
   def lucene
     body = RestClient.get("http://10.27.3.39:9200/_cluster/health?pretty=true").body
     @cluster = JSON.parse(body)
+    @nodes =  JSON.parse(RestClient.get("http://10.27.3.39:9200/_nodes/stats/fs,jvm,http,process?human&pretty&pretty").body)
+
+
     opts = {}
     opts[:category] = params[:category] if  !params[:category].blank?
     opts[:spider_name] = params[:keyword] if !params[:keyword].blank?
