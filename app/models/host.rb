a86792357e897c291f1cf0678adde109d12e8912
yqingp <yqingp@gmail.com>
2018-01-20 11:01:28 +0800

    1

--- app/models/host.rb
+++ app/models/host.rb
@@ -20,8 +20,17 @@ class Host < ApplicationRecord
 	def self.network_environments
 		{"境内"=>1, "境外"=>2}
 	end
-	
+
 	def network_environment_cn
 		Host.network_environments.invert[self.network_environment]
 	end
+
+
+  def self.get_status(heartbeat_at, status)
+    if Time.now - heartbeat_at > 30 && status == "true"
+      return "未知"
+    end
+
+    status == "true" ? "运行中" : "已停止"
+  end
 end
