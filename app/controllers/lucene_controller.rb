19c07f7c1a64a3873b7c17cbcf8fb74813bbcd30
Ray <xulei324@gmail.com>
2018-01-22 12:17:31 +0800

    1

--- app/controllers/lucene_controller.rb
+++ app/controllers/lucene_controller.rb
@@ -1,7 +1,7 @@
 class LuceneController < ApplicationController
   def index
 
-    link = "http://10.27.3.39:9200/_cat/indices/?format=json&pretty&s=docs.count:desc"
+    link = "http://10.27.3.39:9200/_cat/indices/*news*,*weibo*?format=json&pretty&s=docs.count:desc"
     if !params[:category].blank? || !params[:keyword].blank?
       key = params[:category].blank? ? params[:keyword] : params[:category]
       link = "http://10.27.3.39:9200/_cat/indices/*#{key}*?format=json&pretty&s=docs.count:desc"
@@ -12,4 +12,9 @@ class LuceneController < ApplicationController
     @indices = Kaminari.paginate_array(indices, total_count: indices.count).page(params[:page]).per(100)
 
   end
+
+  def create_index
+
+  end
+
 end
