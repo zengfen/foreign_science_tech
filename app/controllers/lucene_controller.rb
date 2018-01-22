d310e9feaa96818861996382db541e40c7c6ebf9
Ray <xulei324@gmail.com>
2018-01-22 12:06:07 +0800

    1

--- app/controllers/lucene_controller.rb
+++ app/controllers/lucene_controller.rb
@@ -1,6 +1,12 @@
 class LuceneController < ApplicationController
   def index
-    body = RestClient.get("http://10.27.3.39:9200/_cat/indices/?format=json&pretty&s=docs.count:desc").body
+
+    link = "http://10.27.3.39:9200/_cat/indices/?format=json&pretty&s=docs.count:desc"
+    unless params[:category].blank? || params[:keyword].blank?
+      key = params[:keyword].blank? ? params[:category] : params[:keyword]
+      link = "http://10.27.3.39:9200/_cat/indices/#{key}?format=json&pretty&s=docs.count:desc"
+    end
+    body = RestClient.get(link).body
     indices = JSON.parse(body)
     @indices = Kaminari.paginate_array(indices, total_count: indices.count).page(params[:page]).per(100)
 
