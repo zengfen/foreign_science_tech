fb0f60581dfa6f667fa7388b1f8f856162a5b0bb
Ray <xulei324@gmail.com>
2018-01-22 11:50:53 +0800

    1

--- app/controllers/lucene_controller.rb
+++ app/controllers/lucene_controller.rb
@@ -1,6 +1,7 @@
 class LuceneController < ApplicationController
   def index
-    body = RestClient.get("http://10.27.3.39:9200/_cat/indices/?format=json&pretty&s=docs.count:desc").body
+    #&s=docs.count:desc
+    body = RestClient.get("http://10.27.3.39:9200/_cat/indices/?format=json&pretty").body
     indices = JSON.parse(body)
     @indices = Kaminari.paginate_array(indices, total_count: indices.count).page(params[:page]).per(100)
 
