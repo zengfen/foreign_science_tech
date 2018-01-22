b57ae14a361f7d047ae693836e91813b208f37d3
Ray <xulei324@gmail.com>
2018-01-22 12:19:36 +0800

    1

--- app/controllers/lucene_controller.rb
+++ app/controllers/lucene_controller.rb
@@ -1,7 +1,7 @@
 class LuceneController < ApplicationController
   def index
 
-    link = "http://10.27.3.39:9200/_cat/indices/*news*,*weibo*?format=json&pretty&s=docs.count:desc"
+    link = "http://10.27.3.39:9200/_cat/indices/#{Spider.categories.values.collect{|x| "*#{x}*"}.join(",")}?format=json&pretty&s=docs.count:desc"
     if !params[:category].blank? || !params[:keyword].blank?
       key = params[:category].blank? ? params[:keyword] : params[:category]
       link = "http://10.27.3.39:9200/_cat/indices/*#{key}*?format=json&pretty&s=docs.count:desc"
