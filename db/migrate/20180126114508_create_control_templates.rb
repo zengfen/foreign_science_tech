4e039e9e7d846a5a15ffdc880ee0db0eda51617e
yqingp <yqingp@gmail.com>
2018-01-26 20:40:41 +0800

    1

--- db/migrate/20180126114508_create_control_templates.rb
+++ db/migrate/20180126114508_create_control_templates.rb
@@ -1,8 +1,12 @@
 class CreateControlTemplates < ActiveRecord::Migration[5.1]
   def change
     create_table :control_templates do |t|
-      t.string :account
-
+      t.string :name
+      t.boolean :is_bind_ip
+      t.integer :window_type
+      t.integer :window_size
+      t.integer :max_count
+      t.boolean :is_absolute_time
       t.timestamps
     end
   end
