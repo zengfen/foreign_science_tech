7c2c7cfaa4440299f2cee2e7d2d0b1e612e52e10
LiQiang <460810619@qq.com>
2018-01-26 15:22:04 +0800

    up

--- app/helpers/monitoring_helper.rb
+++ app/helpers/monitoring_helper.rb
@@ -1,6 +1,17 @@
 module MonitoringHelper
 
 	def pick_up_number(string)
-		return string[/[1-9]\d*\.\d*|0\.\d*[1-9]\d*/] rescue ""
+		float_regular = /[1-9]\d*\.\d*|0\.\d*[1-9]\d*/
+		int_regular = /[1-9]\d*/
+		res = ""
+		begin
+			if string.match(float_regular)
+			 res =  string[float_regular] 
+			elsif string.match(int_regular)
+			 res =  string[int_regular]
+			end
+		rescue
+		end
+		return res
 	end
 end
