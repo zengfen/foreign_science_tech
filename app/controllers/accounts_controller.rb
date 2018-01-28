5811109dea45b3e7ca93d8c455fddacb26df8763
yqingp <yqingp@gmail.com>
2018-01-28 15:51:11 +0800

    1

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,22 @@
 class AccountsController < ApplicationController
   def index
-    @templates = ControlTemplate.select("id, name").collect{|x| [x.name, x.id]}.to_a
+    @templates = ControlTemplate.select('id, name').collect { |x| [x.name, x.id] }.to_a
     @accounts = Account.order('created_at desc').page(params[:page]).per(10)
     @account = Account.new
   end
 
   def new; end
 
-  def create; end
+  def create
+    account_params = params
+                     .require(:account)
+                     .permit(:contents, :control_template_id, :valid_time, :account_type)
+
+    @account = Account.new(account_params)
+    message = @account.save_with_split!
+
+    flash[message.keys.first.to_sym] = message.values.first
+
+    redirect_back(fallback_location: accounts_path)
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -13,4 +13,36 @@
 
 class Account < ApplicationRecord
   attr_accessor :contents
+
+  def self.account_types
+    {
+      0 => '账号',
+      1 => 'Token',
+      2 => 'Cookies'
+    }
+  end
+
+  def account_type_cn
+    self.class.account_types[account_type]
+  end
+
+  def is_valid?
+    Time.now < valid_time ? '有效' : '无效'
+  end
+
+  def save_with_split!
+    return { 'error' => '内容为空' } if contents.blank?
+
+    contents.split("\n").each do |line|
+      next if line.blank?
+
+      account = Account.new(content: line.strip,
+                            account_type: account_type,
+                            valid_time: valid_time,
+                            control_template_id: params[:control_template_id])
+      account.save
+    end
+
+    { 'success' => '保存成功！' }
+  end
 end
