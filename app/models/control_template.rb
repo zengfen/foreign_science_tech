8d5aae43cf3e2598b341739cfa98722fd36a5833
qing <peiyanqing@socialdatamax.com>
2018-01-28 13:31:38 +0800

    1

--- app/models/control_template.rb
+++ app/models/control_template.rb
@@ -2,15 +2,16 @@
 #
 # Table name: control_templates
 #
-#  id               :integer          not null, primary key
-#  name             :string
-#  is_bind_ip       :boolean
-#  window_type      :integer
-#  window_size      :integer
-#  max_count        :integer
-#  is_absolute_time :boolean
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id                  :integer          not null, primary key
+#  name                :string
+#  is_bind_ip          :boolean
+#  window_type         :integer
+#  window_size         :integer
+#  max_count           :integer
+#  is_absolute_time    :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  interval_in_seconds :float
 #
 
 class ControlTemplate < ApplicationRecord
