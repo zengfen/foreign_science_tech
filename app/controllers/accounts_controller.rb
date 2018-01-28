0adde9c69f06c13f8e47b3c794184fa8676d88c6
yqingp <yqingp@gmail.com>
2018-01-28 14:58:48 +0800

    1

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,6 @@
 class AccountsController < ApplicationController
   def index
-    @templates = ControlTemplate.select("id, name").collect{|x| [x.id, x.name]}.to_a
+    @templates = ControlTemplate.select("id, name").collect{|x| [x.name, x.id]}.to_a
     @accounts = Account.order('created_at desc').page(params[:page]).per(10)
     @account = Account.new
   end
