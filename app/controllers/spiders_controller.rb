53cac90fc0a1ba1314c44cfd51622e73a43591c4
yqingp <yqingp@gmail.com>
2018-01-28 20:07:03 +0800

    1

--- app/controllers/spiders_controller.rb
+++ app/controllers/spiders_controller.rb
@@ -1,74 +1,69 @@
 class SpidersController < ApplicationController
-	before_action :logged_in_user
-	before_action :get_spider, only: [:show,:show_info, :load_edit_form, :update, :destroy]
+  before_action :logged_in_user
+  before_action :get_spider, only: %i[show show_info load_edit_form update destroy]
 
-	def index
-		opts = {}
-		opts[:category] = params[:category] if  !params[:category].blank?
-    opts[:spider_name] = params[:keyword] if !params[:keyword].blank?
-		@spiders = Spider.where(opts).order("created_at desc").page(params[:page]).per(10)
-		@spider = Spider.new
+  def index
+    opts = {}
+    opts[:category] = params[:category] unless params[:category].blank?
+    opts[:spider_name] = params[:keyword] unless params[:keyword].blank?
+    @spiders = Spider.where(opts).order('created_at desc').page(params[:page]).per(10)
+    @spider = Spider.new
 
-    @templates = ControlTemplate.select("id, name").collect{|x| [x.name, x.id]}.to_a
-	end
+    @templates = ControlTemplate.select('id, name').collect { |x| [x.name, x.id] }.to_a
+  end
 
+  def show; end
 
-	def show
+  def create
+    @spider = Spider.build_spider(spider_params)
+    if @spider.save
+      flash[:success] = '创建成功'
+    else
+      flash[:error] = @spider.errors.full_messages.join('\n')
+    end
+    redirect_to spiders_path
+  end
 
-	end
+  def load_edit_form; end
 
-	def create
-		@spider = Spider.build_spider(spider_params)
-		if @spider.save
-		 flash[:success] = "创建成功"
-		else
-		 flash[:error] = @spider.errors.full_messages.join('\n')
-		end
-		 redirect_to spiders_path
-	end
-
-	def load_edit_form
-
-	end
-
-	def update
-		update_params = spider_params.clone
-		additional_function = update_params.delete(:additional_function)
+  def update
+    update_params = spider_params.clone
+    additional_function = update_params.delete(:additional_function)
 
     if !additional_function.blank?
-			begin
-				update_params[:additional_function] = JSON.parse(additional_function)
-			rescue
-				@spider.errors.add(:additional_function,"格式错误")
-				flash[:error] = @spider.errors.full_messages.join('\n')
-				redirect_to spiders_path
-				return
-			end
-		else
-			update_params[:additional_function] = []
-		end
-
-		if @spider.update_attributes(update_params)
-      flash[:success] = "更新成功！"
+      begin
+        update_params[:additional_function] = JSON.parse(additional_function)
+      rescue StandardError
+        @spider.errors.add(:additional_function, '格式错误')
+        flash[:error] = @spider.errors.full_messages.join('\n')
+        redirect_to spiders_path
+        return
+      end
     else
-    	flash[:error] = @spider.errors.full_messages.join('\n')
+      update_params[:additional_function] = []
     end
 
-     redirect_to spiders_path
-	end
+    if @spider.update_attributes(update_params)
+      flash[:success] = '更新成功！'
+    else
+      flash[:error] = @spider.errors.full_messages.join('\n')
+    end
 
-	def destroy
-		render json: {type: "success",message:"操作成功！"} if @spider.destroy
-	end
+    redirect_to spiders_path
+  end
 
-	private
+  def destroy
+    render json: { type: 'success', message: '操作成功！' } if @spider.destroy
+  end
 
-	def spider_params
-		params.require(:spider).permit(:spider_name, :spider_type, :network_environment, :proxy_support,:has_keyword,:template_name,:category,:additional_function)
-	end
+  private
 
-	def get_spider
-		@spider = Spider.find_by(:id=>params[:id])
-		redirect_to root_path if @spider.blank?
-	end
+  def spider_params
+    params.require(:spider).permit(:spider_name, :spider_type, :network_environment, :proxy_support, :has_keyword, :template_name, :category, :additional_function, :control_template_id)
+  end
+
+  def get_spider
+    @spider = Spider.find_by(id: params[:id])
+    redirect_to root_path if @spider.blank?
+  end
 end
