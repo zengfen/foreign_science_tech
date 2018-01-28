421d93abefbab00fba34557d532a6bbbc61f1904
yqingp <yqingp@gmail.com>
2018-01-28 13:56:13 +0800

    1

--- db/migrate/20180128055432_change_seconds_to_ms.rb
+++ db/migrate/20180128055432_change_seconds_to_ms.rb
@@ -1,5 +1,6 @@
 class ChangeSecondsToMs < ActiveRecord::Migration[5.1]
   def change
-    change_column :control_templates, :interval_in_seconds, :interval_in_ms, :integer
+    rename_column :control_templates, :interval_in_seconds, :interval_in_ms
+    change_column :control_templates, :interval_in_ms, :integer
   end
 end
