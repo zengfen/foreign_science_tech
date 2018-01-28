2ac078170abbe7755466a4895df6104e91799cc5
yqingp <yqingp@gmail.com>
2018-01-28 14:07:09 +0800

    1

--- app/controllers/control_templates_controller.rb
+++ app/controllers/control_templates_controller.rb
@@ -4,16 +4,18 @@ class ControlTemplatesController < ApplicationController
     @template = ControlTemplate.new
   end
 
-  def new
-  end
-
+  def new; end
 
   def create
-    @control_template = ControlTemplate.new(params[:control_template])
+    control_template_params = params
+                              .require(:control_template)
+                              .permit(:name, :is_bind_ip, :window_size, :window_type, :max_count)
+
+    @control_template = ControlTemplate.new(control_template_params)
     message = @control_template.save_with_calculate!
 
-	  flash[message.keys.first.to_sym] = message.values.first
+    flash[message.keys.first.to_sym] = message.values.first
 
-  	redirect_back(fallback_location: control_templates_path)
+    redirect_back(fallback_location: control_templates_path)
   end
 end
