985ebdce285b722c812a0409a4bdc0c05c2a927c
Ray <xulei324@gmail.com>
2018-01-29 18:51:20 +0800

    1

--- app/controllers/spider_tasks_controller.rb
+++ app/controllers/spider_tasks_controller.rb
@@ -1,16 +1,16 @@
 class SpiderTasksController < ApplicationController
   before_action :logged_in_user
-	before_action :get_spider
+  before_action :get_spider
 
   def index
-		opts = {}
+    opts = {}
     opts[:special_tag] = params[:keyword] if !params[:keyword].blank?
-		@spider_tasks = @spider.spider_tasks.where(opts).order("created_at desc").page(params[:page]).per(10)
-		@spider_task = SpiderTask.new
+    @spider_tasks = @spider.spider_tasks.where(opts).order("created_at desc").page(params[:page]).per(10)
+    @spider_task = SpiderTask.new
   end
 
   def new
-  	@spider_task = SpiderTask.new
+    @spider_task = SpiderTask.new
   end
 
   def show
@@ -18,18 +18,25 @@ class SpiderTasksController < ApplicationController
   end
 
   def create
-  	@spider_task = SpiderTask.new(spider_task_params)
-	  message = 	@spider_task.save_with_spilt_keywords
-	  flash[message.keys.first.to_sym] = message.values.first
+    @spider_task = SpiderTask.new(spider_task_params)
+    add_funs = []
+    params.keys.each do |key|
+      if key.to_s.match(/cate_/)
+        add_funs << {"#{key.gsub("cate_","")}"=>params[key.to_sym]}
+      end
+    end
+    @spider_task.additional_function = add_funs
+    message = @spider_task.save_with_spilt_keywords
+    flash[message.keys.first.to_sym] = message.values.first
 
-  	redirect_back(fallback_location:tasks_path)
+    redirect_back(fallback_location:tasks_path)
   end
 
 
   def start
     @spider_task = SpiderTask.find_by(id: params[:id])
     @spider_task.start!
-    
+
     render :template => "/tasks/refresh_spider_task.js.erb", :layout => false
     #redirect_back(fallback_location:tasks_path)
   end
@@ -38,7 +45,7 @@ class SpiderTasksController < ApplicationController
   def stop
     @spider_task = SpiderTask.find_by(id: params[:id])
     @spider_task.stop!
-    
+
     render :template => "/tasks/refresh_spider_task.js.erb", :layout => false
     # redirect_back(fallback_location:tasks_path)
   end
@@ -47,17 +54,17 @@ class SpiderTasksController < ApplicationController
     @spider_task = SpiderTask.find_by(id: params[:id])
     @spider_task.destroy
 
-   render json: {type: "success",message:"删除成功！"} 
+    render json: {type: "success",message:"删除成功！"}
   end
 
 
   private
   def spider_task_params
-  		params.require(:spider_task).permit(:spider_id,:special_tag, :level, :keyword,:max_retry_count)
+    params.require(:spider_task).permit(:spider_id,:special_tag, :level, :keyword,:max_retry_count)
   end
 
   def get_spider
-  	 @spider = Spider.find_by(id: params[:spider_id])
-  	 redirect_to(root_url) if  @spider.blank?
+    @spider = Spider.find_by(id: params[:spider_id])
+    redirect_to(root_url) if  @spider.blank?
   end
 end
