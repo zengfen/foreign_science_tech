3c19b5a41e5303136ca6b2d3633daadb0f79ed3f
Ray <xulei324@gmail.com>
2018-01-29 16:54:44 +0800

    1

--- db/migrate/20180129152032_add_additional_function_to_spider_task.rb
+++ db/migrate/20180129152032_add_additional_function_to_spider_task.rb
@@ -1,4 +1,4 @@
-class AddAdditionalFunctionToSpiders < ActiveRecord::Migration[5.1]
+class AddAdditionalFunctionToSpiderTask < ActiveRecord::Migration[5.1]
   def change
     add_column :spider_tasks, :additional_function, :json
     add_column :spider_cycle_tasks, :additional_function, :json
