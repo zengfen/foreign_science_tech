5ac882ff4ad6013dc0eb99d497c759eb8a82c1be
Ray <xulei324@gmail.com>
2018-01-29 19:06:20 +0800

    1

--- app/controllers/spider_cycle_tasks_controller.rb
+++ app/controllers/spider_cycle_tasks_controller.rb
@@ -1,24 +1,31 @@
 class SpiderCycleTasksController < ApplicationController
-	before_action :logged_in_user
-	before_action :get_spider
+  before_action :logged_in_user
+  before_action :get_spider
 
   def show
 
   end
 
   def create
-  	@spider_cycle_task = SpiderCycleTask.new(spider_cycle_task_params)
-	  message = 	@spider_cycle_task.save_with_spilt_keywords
-	  flash[message.keys.first.to_sym] = message.values.first
+    @spider_cycle_task = SpiderCycleTask.new(spider_cycle_task_params)
+    add_funs = []
+    params.keys.each do |key|
+      if key.to_s.match(/cate_/)
+        add_funs << {"#{key.gsub("cate_","")}"=>params[key.to_sym]}
+      end
+    end
+    @spider_cycle_task.additional_function = add_funs
+    message =   @spider_cycle_task.save_with_spilt_keywords
+    flash[message.keys.first.to_sym] = message.values.first
 
-  	redirect_back(fallback_location:tasks_path)
+    redirect_back(fallback_location:tasks_path)
   end
 
 
   def start
     @spider_cycle_task = SpiderCycleTask.find_by(id: params[:id])
     @spider_cycle_task.task_job_run!
-    
+
     render :template => "/tasks/refresh_spider_cycle_task.js.erb", :layout => false
     #redirect_back(fallback_location:tasks_path(:taks_type=>0))
   end
@@ -36,16 +43,16 @@ class SpiderCycleTasksController < ApplicationController
     @spider_cycle_task = SpiderCycleTask.find_by(id: params[:id])
     @spider_cycle_task.destroy
 
-    render json: {type: "success",message:"删除成功！"} 
+    render json: {type: "success",message:"删除成功！"}
   end
 
   private
   def spider_cycle_task_params
-  		params.require(:spider_cycle_task).permit(:spider_id,:special_tag, :level, :keyword,:period,:max_retry_count)
+    params.require(:spider_cycle_task).permit(:spider_id,:special_tag, :level, :keyword,:period,:max_retry_count)
   end
 
   def get_spider
-  	 @spider = Spider.find_by(id: params[:spider_id])
-  	 redirect_to(root_url) if  @spider.blank?
+    @spider = Spider.find_by(id: params[:spider_id])
+    redirect_to(root_url) if  @spider.blank?
   end
 end
