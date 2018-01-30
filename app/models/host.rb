1b485b41e08de6c5b576b221c6c7a248a6c99de3
yqingp <yqingp@gmail.com>
2018-01-30 10:48:05 +0800

    1

--- app/models/host.rb
+++ app/models/host.rb
@@ -16,50 +16,43 @@
 #
 
 class Host < ApplicationRecord
-	VALID_IP_REGEX = /\A(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\z/
-  validates :extranet_ip,format: { with: VALID_IP_REGEX }
+  VALID_IP_REGEX = /\A(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\z/
+  validates :extranet_ip, format: { with: VALID_IP_REGEX }
 
   before_create :init_network_environment!
 
-	
-	def init_network_environment!
-		return if extranet_ip.blank?		
-		c = $geo_ip.country(extranet_ip.to_s)
+  def init_network_environment!
+    return if extranet_ip.blank?
+    c = $geo_ip.country(extranet_ip.to_s)
     self.network_environment = c.country_code2 == 'CN' ? 1 : 2
-	end
-
-	def self.network_environments
-		{"境内"=>1, "境外"=>2}
-	end
-
-	def network_environment_cn
-		Host.network_environments.invert[self.network_environment]
-	end
-
-
-  def self.get_status(heartbeat_at, status)
-    if (Time.now.to_i - heartbeat_at) > 60 && status == "true"
-      return "未知"
-    end
-
-    status == "true" ? "运行中" : "已停止"
   end
 
+  def self.network_environments
+    { '境内' => 1, '境外' => 2 }
+  end
+
+  def network_environment_cn
+    Host.network_environments.invert[network_environment]
+  end
+
+  def self.get_status(heartbeat_at, status)
+    return '未知' if (Time.now.to_i - heartbeat_at) > 60 && status == 'true'
+
+    status == 'true' ? '运行中' : '已停止'
+  end
 
   def self.services
     {
-      "agent" => "爬虫节点",
-      "supervisor" => "节点监控器",
-      "controller" => "主机控制器",
-      "dispatcher" => "任务分发器",
-      "loader" => "数据loaders",
-      "receiver" => "数据接收器"
+      'agent' => '爬虫节点',
+      'supervisor' => '节点监控器',
+      'controller' => '主机控制器',
+      'dispatcher' => '任务分发器',
+      'loader' => '数据loaders',
+      'receiver' => '数据接收器'
     }
   end
-
 
   def self.get_service_name(service)
     services[service]
   end
-
 end
