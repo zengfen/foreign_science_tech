2d5d669cc44b4ca739e4e8caa04419fe53f46ff0
qing <peiyanqing@socialdatamax.com>
2018-01-31 13:19:10 +0800

    1

--- app/models/spider_task.rb
+++ app/models/spider_task.rb
@@ -4,20 +4,21 @@
 #
 #  id                   :integer          not null, primary key
 #  spider_id            :integer
-#  level                :integer          default("1")
-#  keyword              :string
+#  level                :integer          default(1)
+#  keyword              :text
 #  special_tag          :string
-#  status               :integer          default("0")
-#  success_count        :integer          default("0")
-#  fail_count           :integer          default("0")
+#  status               :integer          default(0)
+#  success_count        :integer          default(0)
+#  fail_count           :integer          default(0)
 #  refresh_time         :datetime
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
-#  max_retry_count      :integer          default("2")
-#  warning_count        :integer          default("0")
+#  max_retry_count      :integer          default(2)
+#  warning_count        :integer          default(0)
 #  spider_cycle_task_id :integer
-#  task_type            :integer          default("1")
+#  task_type            :integer          default(1)
 #  additional_function  :json
+#  is_split             :boolean          default(FALSE)
 #
 
 class SpiderTask < ApplicationRecord
