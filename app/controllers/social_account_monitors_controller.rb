69adde6e6f95af36710020682ea2774aa2f1be18
yqingp <yqingp@gmail.com>
2018-02-06 14:35:49 +0800

    1

--- app/controllers/social_account_monitors_controller.rb
+++ app/controllers/social_account_monitors_controller.rb
@@ -2,6 +2,7 @@ class SocialAccountMonitorsController < ApplicationController
   def index
     @monitor = SocialAccountMonitor.new
     @spiders = Spider.select('id, spider_name').order('created_at desc').collect { |x| [x.spider_name, x.id] }
+    @monitors = SocialAccountMonitor.all
   end
 
   def create
