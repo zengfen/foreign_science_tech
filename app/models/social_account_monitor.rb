b93789a8ae66cc8ec343c91fb208876bbde1f3fb
qing <peiyanqing@socialdatamax.com>
2018-02-06 13:55:20 +0800

    1

--- app/models/social_account_monitor.rb
+++ app/models/social_account_monitor.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: social_account_monitors
+#
+#  id           :integer          not null, primary key
+#  account_type :string
+#  spider_id    :integer
+#  cycle_type   :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class SocialAccountMonitor < ApplicationRecord
 end
