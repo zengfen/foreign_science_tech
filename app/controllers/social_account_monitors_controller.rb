998b37bfcdc8177bd197db9ea180c132cc76c97e
yqingp <yqingp@gmail.com>
2018-02-06 14:09:51 +0800

    1

--- app/controllers/social_account_monitors_controller.rb
+++ app/controllers/social_account_monitors_controller.rb
@@ -1,6 +1,8 @@
 class SocialAccountMonitorsController < ApplicationController
 
   def index
+    @monitor = SocialAccountMonitor.new
+    @spiders = Spider.select("id, name").collect{|x| [x.name, x.id]}
   end
 
 end
