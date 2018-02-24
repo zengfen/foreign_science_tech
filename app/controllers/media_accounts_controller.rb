a8028f4a41988f5b3481ae0869acab994670ff16
LiQiang <460810619@qq.com>
2018-02-24 16:58:31 +0800

    up

--- app/controllers/media_accounts_controller.rb
+++ app/controllers/media_accounts_controller.rb
@@ -12,15 +12,28 @@ class MediaAccountsController < ApplicationController
   end
 
   def test
-  	$elastic = EsConnect.client
+  	client = EsConnect.client
   	@options = MediaAccount.get_aggs_opts
   	@media_accounts = MediaAccount.order("created_at asc").page(params[:page]).per(20)
   end
 
   def search
-  	$elastic = EsConnect.client
+    page = (params[:page] || 1).to_i
+  	client = EsConnect.client
   	@options = MediaAccount.get_aggs_opts
-  	@media_accounts = MediaAccount.order("created_at asc").page(params[:page]).per(20)
+  	must_opts = []
+  	must_opts<< {term:{fmt: params[:fmt]}} unless params[:fmt].blank?
+  	must_opts<< {term:{slg: params[:slg]}} unless params[:slg].blank?
+  	must_opts<< {query_string:{fields:["name"],query:params[:keyword]}} unless params[:keyword].blank?
+  	res = client.search index:"media_accounts",body:{
+  		  	 size: 20,
+           from: page - 1,
+           query:{ bool:{must:must_opts}},                                       
+           sort: {_uid: {order: "asc" }}
+        }
+    total_count = res["hits"]["total"]
+  	datas = res["hits"]["hits"].collect{|x| Hashie::Mash.new(x["_source"])} rescue []
+  	@media_accounts = Kaminari.paginate_array(datas, total_count: total_count).page(params[:page]).per(20)
   	render :test
   end
 
