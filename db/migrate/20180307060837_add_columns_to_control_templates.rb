b9a1695c2e8db28e4f68b445632a1e90a757b2da
yqingp <yqingp@gmail.com>
2018-03-07 14:20:16 +0800

    1

--- db/migrate/20180307060837_add_columns_to_control_templates.rb
+++ db/migrate/20180307060837_add_columns_to_control_templates.rb
@@ -1,4 +1,7 @@
 class AddColumnsToControlTemplates < ActiveRecord::Migration[5.1]
   def change
+    add_column :control_templates, :has_account, :boolean, default: true, null: false
+    add_column :control_templates, :start_delay, :float
+    add_column :control_templates, :end_delay, :float
   end
 end
