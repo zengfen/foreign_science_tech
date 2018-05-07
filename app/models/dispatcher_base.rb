3a5b37790b55d4c5d6764e301cb83925149f9945
yqingp <yqingp@gmail.com>
2018-05-07 23:22:03 +0800

    1

--- app/models/dispatcher_base.rb
+++ app/models/dispatcher_base.rb
@@ -2,4 +2,11 @@ class DispatcherBase < ActiveRecord::Base
   self.abstract_class = true
   establish_connection DispatcherDB
 
+
+  def self.migrate_data
+    $archon_redis.smembers("archon_available_tasks").each do |x|
+      puts x
+      puts SpiderTask.find(x).max_retry_count
+    end
+  end
 end
