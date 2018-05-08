4fa33cb58cb560d6cfac16ee6b17b9dba463bfdc
yqingp <yqingp@gmail.com>
2018-05-08 11:57:09 +0800

    1

--- db/migrate/20180508035425_add_valid_ips_to_accounts.rb
+++ db/migrate/20180508035425_add_valid_ips_to_accounts.rb
@@ -1,5 +1,5 @@
 class AddValidIpsToAccounts < ActiveRecord::Migration[5.1]
   def change
-    add_column :accounts, :valid_ips, :array, default: [], null: false
+    add_column :accounts, :valid_ips, :text, array: true, default: []
   end
 end
