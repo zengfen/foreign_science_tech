84d47f872d0a1fc2138282f71bde86377b1a7c35
yqingp <yqingp@gmail.com>
2018-05-08 14:20:54 +0800

    1

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,6 +3,22 @@ class AccountsController < ApplicationController
     @templates = ControlTemplate.select('id, name').where(has_account: true).collect { |x| [x.name, x.id] }.to_a
     @accounts = Account.where(control_template_id: @templates.collect{|x| x[1]}).order('created_at desc').page(params[:page]).per(50)
     @account = Account.new
+
+    @ips = DispatcherHost.all
+    agent_ips = DispatcherHostServiceWorker.where(service_name: "agent").group(:ip, :service_name).collect(&:ip)
+
+    @interanl_agents = []
+    @external_agents = []
+
+    @ips.each do |ip|
+      next if agent_ips.include?(ip.ip)
+
+      if ip.is_internal
+        @interanl_agents << ip.ip
+      else
+        @external_agents << ip.ip
+      end
+    end
   end
 
   def new; end
