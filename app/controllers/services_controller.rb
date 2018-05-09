f5b18aa91f811024e8545ffe79916e5143d5fba8
yqingp <yqingp@gmail.com>
2018-05-09 13:18:27 +0800

    1

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,9 +3,6 @@ class ServicesController < ApplicationController
   def index
     ip, service_names = params[:keyword], params[:services]
     @services, @running_services = DispatcherHost.service_details(ip, service_names)
-
-
-    render json: [@services, @running_services]
   end
 
 end
