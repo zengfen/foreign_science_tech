4d41934ca7489fe7ee0940ec9f5c53bbc941b69e
yqingp <yqingp@gmail.com>
2018-05-09 21:45:22 +0800

    1

--- app/models/spider_task.rb
+++ app/models/spider_task.rb
@@ -218,17 +218,14 @@ class SpiderTask < ApplicationRecord
 
   def success_count
     DispatcherSubtaskStatus.where(task_id: id).where('status = 1 or status = 2').count
-    # $archon_redis.scard("archon_completed_tasks_#{id}")
   end
 
   def fail_count
     DispatcherSubtaskStatus.where(task_id: id).where('status = 3').count
-    # $archon_redis.scard("archon_discard_tasks_#{id}")
   end
 
   def warning_count
     DispatcherSubtaskStatus.where(task_id: id).where('status = 3').count
-    # $archon_redis.scard("archon_warning_tasks_#{id}")
   end
 
   def current_total_count
