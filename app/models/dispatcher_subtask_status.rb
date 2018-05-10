40cfa5801268cce7c719993eacf18a0eff5c9fe1
yqingp <yqingp@gmail.com>
2018-05-10 13:49:31 +0800

    1

--- app/models/dispatcher_subtask_status.rb
+++ app/models/dispatcher_subtask_status.rb
@@ -1,6 +1,6 @@
 class DispatcherSubtaskStatus < DispatcherBase
   self.table_name = "subtask_statuses"
 
-  has_one :dispatcher_subtask_status, foreign_key: :id
-  belongs_to :dispatcher_subtask_status, foreign_key: :id
+  has_one :dispatcher_subtask, foreign_key: :id
+  belongs_to :dispatcher_subtask, foreign_key: :id
 end
