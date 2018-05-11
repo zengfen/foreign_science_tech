6872be5ee51fb62a1cac37a8e9d4463d0b2f3cb9
yqingp <yqingp@gmail.com>
2018-05-11 10:51:02 +0800

    1

--- app/models/dispatcher_host.rb
+++ app/models/dispatcher_host.rb
@@ -1,6 +1,5 @@
-class DispatcherHost  < DispatcherBase
-  self.table_name = "hosts"
-
+class DispatcherHost < DispatcherBase
+  self.table_name = 'hosts'
 
   def self.service_names
     {
@@ -10,14 +9,14 @@ class DispatcherHost  < DispatcherBase
       'dispatcher' => '任务分发器',
       'loader' => '数据loaders',
       'receiver' => '数据接收器',
-      "dumper" => '数据导出器',
+      'dumper' => '数据导出器'
     }
   end
 
   def self.internal_agents
     ips = DispatcherHost.where(is_internal: true).collect(&:ip)
     DispatcherHostServiceWorker
-      .where(service_name: "agent", ip: ips)
+      .where(service_name: 'agent', ip: ips)
       .group(:ip, :service_name)
       .collect(&:ip)
   end
@@ -25,11 +24,10 @@ class DispatcherHost  < DispatcherBase
   def self.external_agents
     ips = DispatcherHost.where(is_internal: false).collect(&:ip)
     DispatcherHostServiceWorker
-      .where(service_name: "agent", ip: ips)
+      .where(service_name: 'agent', ip: ips)
       .group(:ip, :service_name)
       .collect(&:ip)
   end
-
 
   def self.list_services
     installed_services = {}
@@ -39,8 +37,7 @@ class DispatcherHost  < DispatcherBase
       installed_services[x.ip] << service_names[x.service_name]
     end
 
-
-    all_service_workers = DispatcherHostServiceWorker.order("last_active_at desc")
+    all_service_workers = DispatcherHostServiceWorker.order('last_active_at desc')
 
     running_services = {}
 
@@ -51,7 +48,6 @@ class DispatcherHost  < DispatcherBase
 
       next if running_services[worker.ip].key?(worker.service_name)
 
-
       running_services[worker.ip][worker.service_name] = Time.now.to_i - worker.last_active_at
 
       if (Time.now.to_i - worker.last_active_at) < 300
@@ -59,15 +55,12 @@ class DispatcherHost  < DispatcherBase
       end
     end
 
-
     [DispatcherHost.all, installed_services, running_service_counter]
   end
 
-
   def network_environment_cn
-    self.is_internal ? "境内" : "境外"
+    is_internal ? '境内' : '境外'
   end
-
 
   def self.service_details(ip, selected_services, online_status)
     hosts = {}
@@ -86,14 +79,12 @@ class DispatcherHost  < DispatcherBase
       installed_services[x.ip] << [hosts[x.ip], x.service_name, x.error_at, x.error_content]
     end
 
-
-    all_service_workers = DispatcherHostServiceWorker.order("last_active_at desc")
+    all_service_workers = DispatcherHostServiceWorker.order('last_active_at desc')
 
     running_services = {}
 
     running_service_counter = {}
     all_service_workers.each do |worker|
-
       next if !ip.blank? && ip != worker.ip
       next if !selected_services.blank? && !selected_services.include?(worker.service_name)
 
@@ -101,31 +92,22 @@ class DispatcherHost  < DispatcherBase
       running_service_counter[worker.ip] ||= {}
 
       next if running_services[worker.ip].key?(worker.service_name)
-      running_services[worker.ip][worker.service_name] = ""
+      running_services[worker.ip][worker.service_name] = ''
 
       last_active_interval = (Time.now.to_i - worker.last_active_at)
 
-      next if !online_status.blank? && online_status == "1" && last_active_interval > 300
-      next if !online_status.blank? && online_status == "0" && last_active_interval < 300
+      next if !online_status.blank? && online_status == '1' && last_active_interval > 300
+      next if !online_status.blank? && online_status == '0' && last_active_interval < 300
 
       running_service_counter[worker.ip][worker.service_name] = [worker.last_active_at, last_active_interval < 300]
     end
 
-
     installed_services.each do |_ip, _services|
-      running_service_counter[_ip] ||= {}
+      running_service_counter[_ip] ||= {} if online_status.blank?
     end
 
-    if !online_status.blank?
-      installed_services.each do |_ip, _services|
-        installed_services.delete(_ip) if _services.blank?
-      end
-    end
 
 
     [installed_services, running_service_counter]
-
   end
-
-
 end
