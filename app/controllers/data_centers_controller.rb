19d14ff0da7dbb166e4ab3b0133fd543b4d8f567
yqingp <yqingp@gmail.com>
2018-05-11 14:27:29 +0800

    1

--- app/controllers/data_centers_controller.rb
+++ app/controllers/data_centers_controller.rb
@@ -11,14 +11,16 @@ class DataCentersController < ApplicationController
     @results = eval((table_name + "_tag").camelize)
 
     if table_name == "archon_facebook_post"
-      @results = @results.includes(record: :user).page(10).per(10)
+      @results = @results.includes(record: :user)
     elsif table_name == "archon_facebook_comment"
-      @results = @results.includes(record:  :user).page(10).per(10)
+      @results = @results.includes(record:  :user)
     elsif table_name == "archon_twitter" || table_name == "archon_weibo"
-      @results = @results.includes(record: [:user, :retweet_user]).page(10).per(10)
+      @results = @results.includes(record: [:user, :retweet_user])
     else
-      @results = @results.includes(:record).page(10).per(10)
+      @results = @results.includes(:record)
     end
+
+    @results = @results.page(params[:page]).per(20)
   end
 
 end
