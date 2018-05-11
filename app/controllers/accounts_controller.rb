6ae35c199152c962f510fd340f11b91e7669cd8d
yqingp <yqingp@gmail.com>
2018-05-11 11:25:22 +0800

    1

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -12,7 +12,7 @@ class AccountsController < ApplicationController
   def create
     account_params = params
     .require(:account)
-    .permit(:contents, :ips, :control_template_id, :valid_time, :account_type)
+    .permit(:contents, :valid_ips, :control_template_id, :valid_time, :account_type)
 
     @account = Account.new(account_params)
     message = @account.save_with_split!
