f4dac4071b3264ae6ca7041ee583565d01164d35
yqingp <yqingp@gmail.com>
2018-05-11 14:37:56 +0800

    1

--- app/controllers/data_centers_controller.rb
+++ app/controllers/data_centers_controller.rb
@@ -1,29 +1,35 @@
 class DataCentersController < ApplicationController
-
   def index
     @results = ArchonBase.table_metas
   end
 
-
   def show
     table_name = params[:id]
 
-
-    @name = ArchonBase.model_mapper.invert[table_name]
-
-    @results = eval((table_name + "_tag").camelize)
-
-    if table_name == "archon_facebook_post"
-      @results = @results.includes(record: :user)
-    elsif table_name == "archon_facebook_comment"
-      @results = @results.includes(record:  :user)
-    elsif table_name == "archon_twitter" || table_name == "archon_weibo"
-      @results = @results.includes(record: [:user, :retweet_user])
-    else
-      @results = @results.includes(:record)
+    unless table_name.blank?
+      @name = ArchonBase.model_mapper.invert[table_name]
+      @results = eval((table_name + '_tag').camelize)
+                 .includes(:record).page(params[:page]).per(20)
     end
-
-    @results = @results.page(params[:page]).per(20)
   end
 
+  def record_details
+    unless table_name.blank?
+      @name = ArchonBase.model_mapper.invert[table_name]
+
+      @results = eval((table_name + '_tag').camelize)
+
+      if table_name == 'archon_facebook_post'
+        @results = @results.includes(record: :user)
+      elsif table_name == 'archon_facebook_comment'
+        @results = @results.includes(record:  :user)
+      elsif table_name == 'archon_twitter' || table_name == 'archon_weibo'
+        @results = @results.includes(record: %i[user retweet_user])
+      else
+        @results = @results.includes(:record)
+      end
+
+      @results = @results.page(params[:page]).per(20)
+    end
+  end
 end
