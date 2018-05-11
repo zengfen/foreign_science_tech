43c49196726e20952f087d000dab24b1f01cef67
yqingp <yqingp@gmail.com>
2018-05-11 11:51:10 +0800

    1

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module ArchonCenter
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.eager_load_paths << Rails.root.join('app/models/index')
+    # config.eager_load_paths << Rails.root.join('app/models/index')
     config.eager_load_paths << Rails.root.join('app/models/archon')
 
     config.autoload_paths += ["#{config.root}/lib"]
