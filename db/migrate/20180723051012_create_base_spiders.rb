f45c012af48613a4eabd77c5e5d751b3f5ac0d2a
yqingp <yqingp@gmail.com>
2018-07-23 13:14:21 +0800

    1

--- db/migrate/20180723051012_create_base_spiders.rb
+++ db/migrate/20180723051012_create_base_spiders.rb
@@ -1,7 +1,10 @@
 class CreateBaseSpiders < ActiveRecord::Migration[5.1]
   def change
     create_table :base_spiders do |t|
-
+      t.string :name
+      t.string :template_name
+      t.string :network_environment
+      t.integer :control_template_id
       t.timestamps
     end
   end
