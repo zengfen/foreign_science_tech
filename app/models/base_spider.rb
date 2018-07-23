07aee837e17d37b7137eec296214676662cb126d
yqingp <yqingp@gmail.com>
2018-07-23 13:21:34 +0800

    1

--- app/models/base_spider.rb
+++ app/models/base_spider.rb
@@ -1,2 +1,9 @@
 class BaseSpider < ApplicationRecord
+  belongs_to :control_template
+
+  def control_template_name
+    return '' if control_template_id.blank?
+
+    control_template.name
+  end
 end
