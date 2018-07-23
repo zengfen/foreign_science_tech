bfe0c0efb250a3644b5d49afec7dedc52e08917f
yqingp <yqingp@gmail.com>
2018-07-23 13:35:33 +0800

    1

--- app/models/base_spider.rb
+++ app/models/base_spider.rb
@@ -23,6 +23,17 @@ class BaseSpider < ApplicationRecord
     network_environment == 1 ? '境内' : '境外'
   end
 
+
+  def self.init_all
+    Spider.all.each do |x|
+      BaseSpider.create(name: x.template_name, network_environment: x.network_environment, control_template_id: x.control_template_id, template_name: x.template_name)
+
+      x.dep_templates.each do |k, v|
+        BaseSpider.create(name: k, network_environment: x.network_environment, control_template_id: v, template_name: k)
+      end
+    end
+  end
+
   # def self.build_spider(spider_params)
   #   spider = BaseSpider.new(spider_params)
 
