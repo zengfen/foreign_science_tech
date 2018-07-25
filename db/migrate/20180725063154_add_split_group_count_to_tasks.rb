18cb62a87757c1d53e718010ae26062c8f75248a
yqingp <yqingp@gmail.com>
2018-07-25 14:32:59 +0800

    1

--- db/migrate/20180725063154_add_split_group_count_to_tasks.rb
+++ db/migrate/20180725063154_add_split_group_count_to_tasks.rb
@@ -1,4 +1,6 @@
 class AddSplitGroupCountToTasks < ActiveRecord::Migration[5.1]
   def change
+    add_column :spider_tasks, :split_group_count, :integer
+    add_column :spider_cycle_tasks, :split_group_count, :integer
   end
 end
