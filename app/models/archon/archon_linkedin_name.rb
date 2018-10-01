e1110e2bfbdb2351fdfe3a167bacbd75b84d57da
yqingp <yqingp@gmail.com>
2018-10-01 21:53:53 +0800

    1

---
+++ app/models/archon/archon_linkedin_name.rb
@@ -0,0 +1,35 @@
+class ArchonLinkedinName < ArchonBase
+  def self.fix_user
+    ids = ArchonLinkedinUser.select("id").collect(&:id)
+    invalid_ids = []
+
+    i = 0
+    ids.each_slice(100).each do |temp_ids|
+      puts i
+      i += 1
+      valid_ids = []
+      temp_ids.each do |x|
+        if x.include?("?") || x.include?("/")
+          invalid_ids << x
+        else
+          valid_ids << x
+        end
+      end
+
+      users = ArchonLinkedinUser.select("id").where(id: valid_ids).where('experience == "" and education == "" and skills == ""').collect(&:id)
+      users0 = valid_ids - users
+
+      results = []
+      users.each do |x|
+        results << {id: x, is_dump: false}
+      end
+
+      users0.each do |x|
+        results << {id: x, is_dump: true}
+      end
+    end
+
+
+    ArchonLinkedinUser.where(id: invalid_ids).delete_all
+  end
+end
