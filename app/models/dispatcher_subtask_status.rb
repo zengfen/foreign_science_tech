566ccb72cf815ff32f996d2d24a301322abaacb9
yqingp <yqingp@gmail.com>
2018-10-02 16:03:54 +0800

    1

--- app/models/dispatcher_subtask_status.rb
+++ app/models/dispatcher_subtask_status.rb
@@ -53,4 +53,42 @@ class DispatcherSubtaskStatus < DispatcherBase
       spider.destroy
     end
   end
+
+  def self.sync_status_new
+    SpiderTask.where(spider_id: 128).each do |spider|
+      next if spider.status != 2
+      tasks = DispatcherSubtaskStatus.where(task_id: spider.id, status: 3)
+      tasks.each do |x|
+        subtask = DispatcherSubtask.find(x.id)
+        content = JSON.parse(subtask.content)['url']
+        if x.error_content == "cookie is expired"
+          spider.retry_fail_task(x.id)
+        end
+        next unless x.error_content == "This profile can't be accessed"
+        name = ArchonLinkedinName.where(id: content).first
+        if name.blank?
+          begin
+            ArchonLinkedinName.create(id: content, is_dump: true, is_invalid: true)
+          rescue Exception => _
+            nil
+          end
+        else
+          name.is_dump = true
+          name.is_invalid = true
+          name.save
+        end
+      end
+
+
+      # if has_expire_cookie
+      #   ControlTemplate.find(66).accounts.each do |x|
+      #     x.valid_time = 1.hour.ago
+      #     x.save
+      #   end
+      #   break
+      # end
+      # spider.destroy
+    end
+  end
+
 end
