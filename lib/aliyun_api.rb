b61433b87d7f9e6225aacc3ff68ce4ed66b798f2
yqingp <yqingp@gmail.com>
2018-10-03 18:48:54 +0800

    1

--- lib/aliyun_api.rb
+++ lib/aliyun_api.rb
@@ -91,7 +91,7 @@ class AliyunApi
     sign_string << CGI.escape(sign_string_new[1..-1])
     # puts sign_string
 
-    signature = OpenSSL::HMAC.digest("sha1", sign_string, "#{api_secret}&").to_s
+    signature = OpenSSL::HMAC.digest("sha1","#{api_secret}&",  sign_string).to_s
     signature = Base64.encode64(signature).strip
     params[:Signature] = signature
 
