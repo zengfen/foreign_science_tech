356cb2941a83c696dcfd3a33149cdc9fb5df4831
yqingp <yqingp@gmail.com>
2018-10-14 15:13:46 +0800

    1

--- app/controllers/service_codes_controller.rb
+++ app/controllers/service_codes_controller.rb
@@ -15,6 +15,16 @@ class ServiceCodesController < ApplicationController
 
   def do_compile
     @code = ServiceCode.where(name: params[:name]).first
+    base_params = params
+    .require(:service_code)
+    .permit(:name, :go_path,
+            :code_path, :current_branch)
+
+
+    @code.do_compile(base_params[:service_code][:go_path], base_params[:service_code][:code_path], base_params[:service_code][:current_branch])
+
+
+    redirect_back(fallback_location: "/service_codes")
   end
 
 
--- app/models/service_code.rb
+++ app/models/service_code.rb
@@ -13,4 +13,13 @@ class ServiceCode < ApplicationRecord
       ServiceCode.create(name: x)
     end
   end
+
+
+  def do_compile(go_path, code_path, current_branch)
+    `cd #{code_path}; git pull; git checkout #{current_branch}; git pull; go install`
+    self.go_path = go_path
+    self.code_path = code_path
+    self.current_branch = current_branch
+    self.save
+  end
 end
