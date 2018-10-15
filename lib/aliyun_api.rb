381a785aa34d6d871764ee51dc0508f874c7f515
yqingp <yqingp@gmail.com>
2018-10-15 17:12:49 +0800

    1

--- lib/aliyun_api.rb
+++ lib/aliyun_api.rb
@@ -18,7 +18,7 @@ class AliyunApi
     return make_request({:Action => "RunInstances",
                          :RegionId => "us-west-1",
                          :ZoneId => "us-west-1b",
-                         :ImageId => "m-rj9ec5tekle8wz2clc74",
+                         :ImageId => "s-rj9f4pyrts4gu05lf0zf",
                          :InstanceType => "ecs.n1.tiny",
                          :SecurityGroupId => "sg-u16i9xyyd",
                          :InstanceChargeType => "PostPaid",
