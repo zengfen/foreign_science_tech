ecb0c4f92b46d20a664f773d18e6938da21fdbec
yqingp <yqingp@gmail.com>
2018-10-16 14:30:42 +0800

    1

--- app/models/archon/archon_wikidata.rb
+++ app/models/archon/archon_wikidata.rb
@@ -1,4 +1,19 @@
 class ArchonWikidata < ArchonBase
+
+  def self.update_social(id, fb, tw)
+    x = ArchonWikidata.find(id)
+    if !fb.blank?
+      x.facebook = [fb].to_json
+    end
+
+    if !tw.blank?
+      x.twitter = [tw].to_json
+    end
+
+    x.save
+  end
+
+
 end
 
 
