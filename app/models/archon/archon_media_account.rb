7c5481f646bb2b5e3f9763c1fa4c7f7176559a7d
yqingp <yqingp@gmail.com>
2018-10-17 22:07:05 +0800

    1

---
+++ app/models/archon/archon_media_account.rb
@@ -0,0 +1,18 @@
+class ArchonMediaAccount  < ArchonBase
+  def self.load
+    records = []
+    File.new("db/media_account.txt").each do |line|
+      next if line.blank?
+      records << self.new(JSON.parse(line))
+      if records.size == 1000
+        self.import records
+
+        records = []
+      end
+    end
+
+    if records.size > 0
+      self.import records
+    end
+  end
+end
---
+++ app/models/archon/archon_social_account.rb
@@ -0,0 +1,3 @@
+class ArchonSocialAccount  < ArchonBase
+
+end
