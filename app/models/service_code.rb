f3f68f3423620e205461c3f47f2e1be74a9498e1
yqingp <yqingp@gmail.com>
2018-10-18 22:27:14 +0800

    1

--- app/models/service_code.rb
+++ app/models/service_code.rb
@@ -15,11 +15,28 @@ class ServiceCode < ApplicationRecord
   end
 
 
-  def do_compile(go_path, code_path, current_branch)
-    `cd #{code_path}; git pull; git checkout #{current_branch}; git pull; go install`
+  def do_compile(go_path, code_path, current_branch, config_content)
+    if self.name == "archon_agent" || self.name == "archon_supervisor"
+      content <<-CONF
+package config
+
+var ConfigContent = `
+#{config_content}
+`
+      CONF
+      `cd #{code_path}; git pull; git checkout #{current_branch}; git pull;`
+
+      File.open("#{code_path}/config/default.go", "w") do |file|
+        file.puts config
+      end
+
+      `cd #{code_path}; go install`
+    end
+
     self.go_path = go_path
     self.code_path = code_path
     self.current_branch = current_branch
+    self.config_content = config_content
     self.save
   end
 end
