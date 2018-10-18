e5d678555d8927728c8069ab1f6df29b0422fe36
yqingp <yqingp@gmail.com>
2018-10-18 23:08:01 +0800

    1

--- config/application.rb
+++ config/application.rb
@@ -47,19 +47,19 @@ rescue Exception => e
 end
 
 
-$e_client = Aliyun::OSS::Client.new(
-  :endpoint => 'http://oss-us-west-1.aliyuncs.com',
-  :access_key_id => 'ZeYymZ4l5LYsMdCe',
-  :access_key_secret => 'k07G51MNpN0GeGfQ8Kru1pzg8FG1ny')
-AliyunEBucket = "archon-binary-0"
+# $oss_client = Aliyun::OSS::Client.new(
+#   :endpoint => 'http://oss-us-west-1.aliyuncs.com',
+#   :access_key_id => 'ZeYymZ4l5LYsMdCe',
+#   :access_key_secret => 'k07G51MNpN0GeGfQ8Kru1pzg8FG1ny')
 
 
-$i_client = Aliyun::OSS::Client.new(
+$oss_client = Aliyun::OSS::Client.new(
   :endpoint => 'http://oss-cn-hangzhou.aliyuncs.com',
   :access_key_id => 'ZeYymZ4l5LYsMdCe',
   :access_key_secret => 'k07G51MNpN0GeGfQ8Kru1pzg8FG1ny')
 
 AliyunIBucket = "archon-binary-1"
+AliyunEBucket = "archon-binary-0"
 
 
 
