bf6a8c8070e58ae977eba04aebd565e869861f0e
yqingp <yqingp@gmail.com>
2018-11-01 19:12:25 +0800

    1

--- app/models/archon/archon_twitter.rb
+++ app/models/archon/archon_twitter.rb
@@ -1,4 +1,18 @@
 class ArchonTwitter < ArchonBase
   belongs_to :user, foreign_key: :user_id, class_name: "ArchonTwitterUser"
   belongs_to :retweet_user, foreign_key: :retweet_user_id, class_name: "ArchonTwitterUser"
+
+
+  def self.dump_twitter_ids
+    user_id = []
+    ArchonTwitter.select("id,user_id").where("place != ''").each do |x|
+      user_id << x.user_id
+      $redis.sadd("dump_twitter_ids_for_fix", x.id)
+    end
+
+
+    puts user_id.uniq.size
+
+    nil
+  end
 end
