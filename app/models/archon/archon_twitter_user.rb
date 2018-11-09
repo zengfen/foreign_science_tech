75b0675bf18b98e14dd74ca46f113096c87a3401
yqingp <yqingp@gmail.com>
2018-11-09 21:09:20 +0800

    1

--- app/models/archon/archon_twitter_user.rb
+++ app/models/archon/archon_twitter_user.rb
@@ -1,3 +1,22 @@
 class ArchonTwitterUser < ArchonBase
+  def self.fix_screen_name
+    start_id = 0
+    end_id = 0
 
+
+    while true
+      ids = ArchonTwitterUser.select("id").where("id > #{start_id}").order("id asc").limit(20000).collect(&:id)
+      break if ids.blank?
+
+      start_id = ids.first
+      puts start_id
+      end_id = ids.last
+      puts end_id
+
+      ArchonTwitterUser.where("id >= #{start_id} and id <=#{end_id}").update_all("screen_name_lower = lower(screen_name)")
+
+
+      start_id = end_id
+    end
+  end
 end
