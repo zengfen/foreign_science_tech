5bfc02bf1e5a50184c15fee9285c8745c846ee96
yqingp <yqingp@gmail.com>
2018-11-09 21:47:34 +0800

    1

--- app/models/archon/archon_twitter_user.rb
+++ app/models/archon/archon_twitter_user.rb
@@ -19,4 +19,45 @@ class ArchonTwitterUser < ArchonBase
       start_id = end_id
     end
   end
+
+
+  def self.check_create_tasks(file)
+    ids = []
+
+    File.new(file).each do |x|
+      next if x.blank?
+
+      ids << x.strip.downcase
+    end
+
+    ids.uniq!
+
+
+    new_ids = []
+    ids.each_slice(3000).each do |temp_ids|
+      names = ArchonTwitterUser.select("screen_name_lower").where(screen_name_lower: temp_ids).collect(&:screen_name_lower)
+
+      new_ids += (temp_ids - names)
+    end
+
+
+    new_ids.each_slice(3000).each do |temp_ids|
+      spider_task = SpiderTask.new(
+        spider_id: 100,
+        level: 1,
+        max_retry_count: 0,
+        keyword: temp_ids.each_slice(100).to_a.collect { |x| x.join('|') }.join(','),
+        special_tag: '',
+        status: 0,
+        timeout_second: 10,
+        task_type: 2,
+        is_split: false,
+        additional_function: [{ 'by_screen_name' => '1' }, { 'has_friends' => '0' }]
+      )
+      spider_task.special_tag_transfor_id
+      spider_task.save_with_spilt_keywords
+      spider_task.start!
+    end
+
+  end
 end
