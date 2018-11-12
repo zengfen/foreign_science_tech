fae5a8f674820dfafecdcaad4686566cd4ee4ef8
Ray <xulei324@gmail.com>
2018-11-12 18:46:09 +0800

    1

--- app/models/archon/archon_linkedin_user_tag.rb
+++ app/models/archon/archon_linkedin_user_tag.rb
@@ -1,11 +1,3 @@
-class ArchonThinkTank < ArchonBase
-
-  def self.load_data
-    File.open("/root/data.js","r").readlines.each do |line|
-      doc = JSON.parse(line)
-      id = Digest::MD5.hexdigest("#{doc["title"]}#{doc["created_at"]}")
-      ArchonThinkTank.create({id:id,title:doc["title"],site:doc["site"],country:doc["country"],desp:doc["desp"],title_cn:doc["title_cn"],file_url:doc["pdf_url"],author:doc["author"],created_at:Time.parse(doc["created_at"]).to_i})
-      ArchonThinkTankTag.create({pid:id,tag:236,created_at:Time.now.to_i}})
-    end
-  end
+class ArchonLinkedinUserTag < ArchonBase
+  belongs_to :archon_linkedin_users , foreign_key: :pid
 end
