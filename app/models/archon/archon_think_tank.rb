d213f3ea8616488e61cca5f1613e74b5767ca0c7
Ray <xulei324@gmail.com>
2018-11-12 19:29:01 +0800

    1

--- app/models/archon/archon_think_tank.rb
+++ app/models/archon/archon_think_tank.rb
@@ -4,7 +4,7 @@ class ArchonThinkTank < ArchonBase
     File.open("/root/data.js","r").readlines.each do |line|
       doc = JSON.parse(line)
       id = Digest::MD5.hexdigest("#{doc["title"]}#{doc["created_at"]}")
-      ArchonThinkTank.create({id:id,title:doc["title"],site:doc["site"],country:doc["country"],desp:doc["desp"],title_cn:doc["title_cn"],file_url:doc["pdf_url"],author:doc["author"],created_at:Time.parse(doc["created_at"]).to_i})
+      ArchonThinkTank.create({id:id,title:doc["title"],site:doc["site"],country:doc["country"],desp:doc["desp"],title_cn:doc["title_cn"],file_url:doc["pdf_url"],author:doc["author"],source_url:doc["source_url"],created_at:Time.parse(doc["created_at"]).to_i})
       ArchonThinkTankTag.create({pid:id,tag:236,created_at:Time.now.to_i})
     end
   end
