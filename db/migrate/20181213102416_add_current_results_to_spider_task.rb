1dc7de4df73bdb5391849968042239c601c6e78d
yqingp <yqingp@gmail.com>
2018-12-13 09:39:17 +0800

    1

--- db/migrate/20181213102416_add_current_results_to_spider_task.rb
+++ db/migrate/20181213102416_add_current_results_to_spider_task.rb
@@ -1,5 +1,7 @@
-class AddConfigContentToServiceCode1 < ActiveRecord::Migration[5.1]
+class AddCurrentResultsToSpiderTask < ActiveRecord::Migration[5.1]
   def change
     add_column :spider_tasks, :current_result_count, :integer, default: 0, null: false
   end
 end
+
+
