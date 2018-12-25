2b5df83e0d1369d861a495bf4f531ac681107330
Ray <xulei324@gmail.com>
2018-12-25 11:22:05 +0800

    1

--- app/models/archon/archon_twitter.rb
+++ app/models/archon/archon_twitter.rb
@@ -1,6 +1,6 @@
 class ArchonTwitter < ArchonBase
-  #belongs_to :user, foreign_key: :user_id, class_name: "ArchonTwitterUser"
-  #belongs_to :retweet_user, foreign_key: :retweet_user_id, class_name: "ArchonTwitterUser"
+  belongs_to :user, foreign_key: :user_id, class_name: "ArchonTwitterUser"
+  belongs_to :retweet_user, foreign_key: :retweet_user_id, class_name: "ArchonTwitterUser"
 
 
   def self.dump_text
