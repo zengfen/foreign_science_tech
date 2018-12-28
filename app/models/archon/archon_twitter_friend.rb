7c64eb728e71cce2c67a8e6cd5815b62373f5c4d
zengfen <zengfen@xjgreat.com>
2018-12-28 19:39:58 +0800

    facebook写入文件方法修改

--- app/models/archon/archon_twitter_friend.rb
+++ app/models/archon/archon_twitter_friend.rb
@@ -2,8 +2,9 @@ class ArchonTwitterFriend < ArchonBase
 
   def self.get_twittwer_followers(user_id)
     follower = []
-    friend_ids = ArchonTwitterFriend.find(id: user_id).friend_ids.split(",")
-    ArchonTwitterUser.find(friend_ids).each do |x|
+    friend_ids = ArchonTwitterFriend.find(id: user_id).friend_ids.split(",") rescue []
+    return [] if friend_ids.blank?
+    ArchonTwitterUser.where(id: friend_ids).each do |x|
       follower << {
         "userId": x.id, #Follower ID，即当前操作者ID（用于标定followeing和操作者的关系）
         "userName": x.name, #Follower Name，即当前操作者名称
