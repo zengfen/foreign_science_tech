eda3e7bfa997b7d9db9826930e5338664e7f0535
lijinmin <lijinmin@xjgreat.com>
2019-02-11 11:33:16 +0800

    m

--- db/migrate/20190211094232_create_media_infos.rb
+++ db/migrate/20190211094232_create_media_infos.rb
@@ -4,7 +4,7 @@ class CreateMediaInfos < ActiveRecord::Migration[5.1]
       t.integer :level, comment: '级别'
       t.string :ch_name, comment: '媒体中文名'
       t.string :en_name, comment: '媒体英文名'
-      t.integer :country_code, comment: '国家代码'
+      t.string :country_code, comment: '国家代码'
       t.string :site, comment: '网址（URL）'
       t.string :domain, comment: '域名'
       t.string :data_source, comment: '数据来源'
