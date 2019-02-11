4dbd6c23795fe879f2fde00fb5e0ddec18e97288
lijinmin <lijinmin@xjgreat.com>
2019-02-11 16:57:37 +0800

    m

--- app/controllers/information_statistics_controller.rb
+++ app/controllers/information_statistics_controller.rb
@@ -2,6 +2,10 @@ class InformationStatisticsController < ApplicationController
   before_action :logged_in_user
 
   def index
-  	
+  	opt = {}
+  	a = InformationStatistics.new
+  	@redis_key = a.redis_key
+  	@day = 0
+  	@lists = MediaInfo.where(opt).page(params[:page]).per(20)
   end
 end
