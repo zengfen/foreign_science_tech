2c2262af956d58b5fc7923aa79b3e21f2ab3df45
zengfen <zengfen@xjgreat.com>
2019-02-12 11:08:25 +0800

    flash修改

--- app/controllers/information_statistics_controller.rb
+++ app/controllers/information_statistics_controller.rb
@@ -30,7 +30,7 @@ class InformationStatisticsController < ApplicationController
 
   def update_statistic
 		UpdateInformationStatisticsJob.perform_later
-		flash[:status] = "更新计算后台运行开始"
+		flash[:notice] = "更新计算后台运行开始"
 		redirect_to information_statistics_path
   end
 
