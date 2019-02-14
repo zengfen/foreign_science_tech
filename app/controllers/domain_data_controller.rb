d1ac468016c9af04d407e882be589eb0f4d6a1ab
xiaomengting <xiaomengting@xjgreat.com>
2019-02-14 17:50:18 +0800

    m

--- app/controllers/domain_data_controller.rb
+++ app/controllers/domain_data_controller.rb
@@ -5,7 +5,9 @@ class DomainDataController < ApplicationController
     page = (params[:page] || 1).to_i
     per_page = (params[:per] || 50).to_i
     keyword = params[:keyword]
-    @lists = DomainDataSource.where("domain like ?", "%#{params[:keyword]}%")page(page).per(per_page)
+    @lists = DomainDataSource.all
+    @lists = @lists.where("domain like ?", "%#{params[:keyword]}%") unless keyword.blank?
+    @lists = @lists.page(page).per(per_page)
     if request.xhr?
      return render "_list_body.html.erb", layout: false
     end
