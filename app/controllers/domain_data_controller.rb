a0f0432b9996b85ff53c7d780dca95bdd09f4f9f
xiaomengting <xiaomengting@xjgreat.com>
2019-02-14 17:41:47 +0800

    m

--- app/controllers/domain_data_controller.rb
+++ app/controllers/domain_data_controller.rb
@@ -4,11 +4,11 @@ class DomainDataController < ApplicationController
   def index
     page = (params[:page] || 1).to_i
     per_page = (params[:per] || 50).to_i
-		@domain_datas = DomainDataSource.page(page).per(per_page)
+    @lists = DomainDataSource.page(page).per(per_page)
     if request.xhr?
      return render "_list_body.html.erb", layout: false
     end
-	end
+  end
 
 
 
