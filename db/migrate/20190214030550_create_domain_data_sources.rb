fec28137e7fae8bf3488cf993628a494e0b77c50
lxyu1993 <lxyu1993@yeah.net>
2019-02-14 11:10:15 +0800

    l

--- db/migrate/20190214030550_create_domain_data_sources.rb
+++ db/migrate/20190214030550_create_domain_data_sources.rb
@@ -1,6 +1,9 @@
 class CreateDomainDataSources < ActiveRecord::Migration[5.1]
   def change
     create_table :domain_data_sources do |t|
+      t.string :domain, comment: '域名'
+      t.text :rss_site, comment: 'RSS地址'
+      t.string :newslookup, comment: 'newslookup id'
 
       t.timestamps
     end
