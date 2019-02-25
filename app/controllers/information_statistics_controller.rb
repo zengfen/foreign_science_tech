35b749152a4a23fda9bed1a940e329e0b4fbafe4
lijinmin <lijinmin@xjgreat.com>
2019-02-25 10:10:19 +0800

    m

--- app/controllers/information_statistics_controller.rb
+++ app/controllers/information_statistics_controller.rb
@@ -181,4 +181,61 @@ class InformationStatisticsController < ApplicationController
     return render json: {type:'success',message:'数据源更新成功'}
   end
 
+  # 下载每日新闻量
+  def download_daily_news_count
+    opt = {}
+    source_opt =  params[:data_source].present? ? "data_source like '%#{params[:data_source]}%'" : {}
+    opt[:country_code] = params[:country_code] if params[:country_code].present?
+    # opt[:level] = params[:level] if params[:level].present?
+    level_opt = params[:level].present? ? "level like '%#{params[:level]}%'" : {}
+    opt[:hav_infos] = params[:hav_infos] if params[:hav_infos].present?
+    opt[:domain] = custom_domains[params[:custom_domains]] if params[:custom_domains].present?
+    name_domain_opt = params[:keyword].present? ? "ch_name like '%#{params[:keyword]}%' or domain like '%#{params[:keyword]}%'" : {}    
+    
+    model = Object.const_get params[:model] 
+    lists = model.where(opt).where(source_opt).where(level_opt).where(name_domain_opt)
+    start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : Date.today
+    end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : Date.today
+    info_count = {}
+    lists.each do |obj|
+      info_count[obj.id] ||= 0
+      # ((Date.today - end_date).to_i..(Date.today - start_date).to_i).each do |day|
+      #   info_count[obj.id] += $redis.hget(@redis_key,"#{obj.domain}_#{day}").to_i
+      # end
+      (start_date..end_date).each do |day|
+        info_count[obj.id] += $redis.hget(@redis_key,"#{obj.domain}_#{day.strftime("%F")}").to_i
+      end
+    end
+    min_count = params[:min_count].present? ? params[:min_count].to_i : 0
+    @info_count = info_count.select{|k,v| params[:max_count].present? ? (v >= min_count && v <= params[:max_count].to_i) : (v >= min_count)}.sort_by{|k,v| v}.reverse.to_h
+    ids = @info_count.keys
+
+    filename = "#{Time.now}_.xlsx"
+    file_path = Rails.root.to_s + "/public/download/#{filename}"
+    workbook  = WriteXLSX.new(file_path)
+    worksheet = workbook.add_worksheet('Sheet1')
+    worksheet.set_column(0, 0, 40)
+    worksheet.set_column(1, 1, 25)
+    worksheet.set_column(2, 2, 25)
+    xlsx_format = workbook.add_format(
+      :size  => 10,
+      :border => 1
+      )
+    xlsx_format.set_align('left')
+
+    i = 0
+    lists.where({id:ids}).each do |x|
+      (start_date..end_date).each do |day|
+        count = $redis.hget(@redis_key,"#{obj.domain}_#{day.strftime("%F")}").to_i
+        date = day.strftime("%F")
+        worksheet.write(i,0,x.ch_name,xlsx_format)
+        worksheet.write(i,1,date,xlsx_format)
+        worksheet.write(i,2,count,xlsx_format)
+        i += 1
+      end      
+    end
+
+    send_file(file_path, :type => 'xlsx', :disposition => 'attachment', :filename => filename)
+  end
+
 end
