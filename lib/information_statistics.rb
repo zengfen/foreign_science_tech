065c0fc3d04ced784082c222511828f108ad3694
lijinmin <lijinmin@xjgreat.com>
2019-02-28 10:24:02 +0800

    m

--- lib/information_statistics.rb
+++ lib/information_statistics.rb
@@ -17,25 +17,31 @@ class InformationStatistics
 				# pids = ArchonNewsTag.where("created_at > ?",start_time).where("created_at <= ?",end_time).pluck('pid')
 				
 				ArchonNews.where("created_time > ?",start_time).where("created_time <= ?",end_time).each do |x|
-					source_url = x.source_url
-					domain = source_url.split('://').last.split('/').first rescue ''
-					domain = domain.gsub('www.','') rescue ''
-					unless domain.blank?
-						created_time = Time.at(x.created_time).strftime("%F")
-						# day_key = (Time.now.to_i - Time.parse(created_time).to_i)/86400
-						day_key = created_time
-						domain_key = "#{domain}_#{day_key}"
-						domain_key1 = "#{x.site}_#{day_key}"
-						domain_key2 = "#{x.site_url}_#{day_key}"
-						$redis.hincrby(redis_key,domain_key,1)
-						if domain_key1 != domain_key
-							$redis.hincrby(redis_key,domain_key1,1)
-						end
+					puts "========start======"
+					# source_url = x.source_url
+					# domain = source_url.split('://').last.split('/').first rescue ''
+					# domain = domain.gsub('www.','') rescue ''
+					# unless domain.blank?
+					# 	created_time = Time.at(x.created_time).strftime("%F")
+					# 	# day_key = (Time.now.to_i - Time.parse(created_time).to_i)/86400
+					# 	day_key = created_time
+					# 	domain_key = "#{domain}_#{day_key}"
+					# 	domain_key1 = "#{x.site}_#{day_key}"
+					# 	domain_key2 = "#{x.site_url}_#{day_key}"
+					# 	$redis.hincrby(redis_key,domain_key,1)
+					# 	if domain_key1 != domain_key
+					# 		$redis.hincrby(redis_key,domain_key1,1)
+					# 	end
 
-						if !domain_key2.include?('/') && domain_key2 != domain_key && domain_key2 != domain_key1
-							$redis.hincrby(redis_key,domain_key2,1)
-						end
-					end
+					# 	if !domain_key2.include?('/') && domain_key2 != domain_key && domain_key2 != domain_key1
+					# 		$redis.hincrby(redis_key,domain_key2,1)
+					# 	end
+					# end
+					created_time = Time.at(x.created_time).strftime("%F")
+					day_key = created_time
+					domain_key2 = "#{x.site_url}_#{day_key}"
+					$redis.hincrby(redis_key,domain_key2,1)
+					puts "=========end==========="
 				end			
 			end			
 		rescue Exception => e
