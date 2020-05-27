0be25b13f1c00cecf906c3df07ec66cabcd1bd9c
zengfen2 <zengfen@china-revival.com>
2020-05-27 15:53:00 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#data_index'
+  root to: 'home#index'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
@@ -15,10 +15,7 @@ Rails.application.routes.draw do
 
 
   resources :home do
-    collection do
-      get :data_index
-      get :monitor_index
-    end
+
   end
 
   resources :spiders do
