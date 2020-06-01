b08d37d725e0c1794a976fe5c0438702ab3e5260
wangxiong <wangxiong@china-revival.com>
2020-06-01 10:59:18 +0800

    wn

--- app/controllers/data_centers_controller.rb
+++ app/controllers/data_centers_controller.rb
@@ -2,5 +2,15 @@ class DataCentersController < ApplicationController
   before_action :logged_in_user
 
   def index
+    @data = [
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {},
+      {}
+    ]
   end
 end
