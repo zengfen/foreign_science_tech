81d052b19b70efa9720b2c0235f139f477045683
zengfen2 <zengfen@china-revival.com>
2020-06-02 14:56:12 +0800

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,11 @@ require 'sidekiq'
 redis_options = Setting.redis.deep_symbolize_keys
 begin
   Sidekiq.configure_server do |config|
-    config.redis = { url: "redis://#{redis_options[:host]}:#{redis_options[:port]}/#{redis_options[:db]}", namespace: 'foreign_science_tech' }
+    config.redis = { url: "redis://#{redis_options[:host]}:#{redis_options[:port]}/#{redis_options[:db]}", password: redis_options[:password], namespace: 'foreign_science_tech' }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: "redis://#{redis_options[:host]}:#{redis_options[:port]}/#{redis_options[:db]}", namespace: 'foreign_science_tech' }
+    config.redis = { url: "redis://#{redis_options[:host]}:#{redis_options[:port]}/#{redis_options[:db]}", password: redis_options[:password], namespace: 'foreign_science_tech' }
   end
 
 rescue Exception => e
