edcd2e1bce175f10258f43168f1f14903450035a
zengfen2 <zengfen@china-revival.com>
2020-06-02 15:05:30 +0800

    fix

--- app/jobs/t_sk_job_instances_job.rb
+++ app/jobs/t_sk_job_instances_job.rb
@@ -4,7 +4,7 @@ class TSkJobInstancesJob < ApplicationJob
   def perform(*args)
     puts "==spider_name====#{args[0]["spider_name"]}="
     spider = Spider.where(spider_name:args[0]["spider_name"]).first
-    res,spider_task = spider.create_spider_task(SpiderTask::CyceTask)
+    res,spider_task = spider.create_spider_task(SpiderTask::CycleTask)
     if res[:type] == "success"
       res2 = spider_task.start_task
     end
