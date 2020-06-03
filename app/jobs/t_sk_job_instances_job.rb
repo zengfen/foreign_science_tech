903586676f4a08ff0eb34b86d6df9411c3d0fe85
zengfen2 <zengfen@china-revival.com>
2020-06-03 12:55:37 +0800

    fix

--- app/jobs/t_sk_job_instances_job.rb
+++ app/jobs/t_sk_job_instances_job.rb
@@ -2,12 +2,21 @@ class TSkJobInstancesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts "==spider_name====#{args[0]["spider_name"]}="
+    puts "==spider_name====#{args[0]["spider_name"]}==#{args[0]}=="
+
+    # if SpiderTask.where("created_at >= ? and task_type = ?",Date.today,SpiderTask::CycleTask).blank?
+    #   spider = Spider.where(spider_name: args[0]["spider_name"]).first
+    #   _, spider_task = spider.create_spider_task(SpiderTask::CycleTask)
+    # end
+    #
+    # SpiderTask.where(status: [SpiderTask::TypeTaskStart, SpiderTask::TypeTaskStop]).each do |spdier_task|
+    #   res2 = spider_task.start_task
+    # end
+
     spider = Spider.where(spider_name:args[0]["spider_name"]).first
     res,spider_task = spider.create_spider_task(SpiderTask::CycleTask)
     if res[:type] == "success"
       res2 = spider_task.start_task
     end
-
   end
 end
--- app/models/common/t_sk_job_instance.rb
+++ app/models/common/t_sk_job_instance.rb
@@ -5,7 +5,7 @@ class TSkJobInstance < CommonBase
   # 今天没有执行过周期任务，且周期时间改为比当前时间早，则需要立即执行周期任务
   def need_enque!
     spider  = Spider.where(spider_name:self.spider_name).first
-    last_execute_time = spider.spider_tasks.where("created_at >= ?",Date.today).order(:created_at).last.created_at.to_i rescue nil
+    last_execute_time = spider.spider_tasks.where("created_at >= ? and task_type = ?",Date.today,SpiderTask::CycleTask).order(:created_at).last.created_at.to_i rescue nil
     return true if last_execute_time.blank?
     cron_time = Time.parse("#{self.cron_hour}:#{self.cron_minutes}").to_i rescue nil
     if last_execute_time < Date.today.to_time.to_i && cron_time < last_execute_time
@@ -21,8 +21,9 @@ class TSkJobInstance < CommonBase
     # return if self.status_before_type_cast != 4 || self.is_deleted
     cron = Sidekiq::Cron::Job.find self.job_name
     if cron.blank?
+      puts "========111111111"
       # time = "#{self.cron_minutes} #{self.cron_hour} * * * Asia/Shanghai"
-      time = "* */1 * * * Asia/Shanghai"
+      time = "* * */1 * * Asia/Shanghai"
       cron = Sidekiq::Cron::Job.new(name: self.job_name, cron: time, class: 'TSkJobInstancesJob', args: {spider_name: self.spider_name}) if cron.blank?
       if cron.valid?
         cron.save
@@ -31,9 +32,10 @@ class TSkJobInstance < CommonBase
         Rails.logger.info cron.errors
       end
     else
-      time = "* */1 * * * Asia/Shanghai"
-      cron.cron = time
-      cron.save
+      puts "========22222222222"
+      # time = "* */1 * * * Asia/Shanghai"
+      # cron.cron = time
+      # cron.save
       cron.enque! if need_enque!
     end
   end
