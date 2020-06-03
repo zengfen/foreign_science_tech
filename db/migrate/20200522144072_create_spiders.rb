33ac1309adedd6068c26188488fa1cea226ccf8a
zengfen2 <zengfen@china-revival.com>
2020-06-03 15:35:29 +0800

    fix

--- db/migrate/20200522144072_create_spiders.rb
+++ db/migrate/20200522144072_create_spiders.rb
@@ -1,7 +1,7 @@
 class CreateSpiders < ActiveRecord::Migration[5.0]
   def change
     create_table :spiders do |t|
-      t.string 'spider_name'
+      t.string 'spider_name', :limit => 50
       t.string 'name_cn'
       t.string 'name_en'
       t.integer 'status',default:0
