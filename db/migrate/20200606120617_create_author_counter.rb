9e1528c44fb7d6cf5c6000731778e444777d7cdf
zengfen2 <zengfen@china-revival.com>
2020-06-07 18:58:04 +0800

    fix

--- db/migrate/20200606120617_create_author_counter.rb
+++ db/migrate/20200606120617_create_author_counter.rb
@@ -1,9 +1,9 @@
 class CreateAuthorCounter < ActiveRecord::Migration[5.0]
   def change
     create_table :author_counters do |t|
-      t.string :author_name
+      t.string :author_name, :limit => 50
       t.integer :count, default: 0, null: false
-      t.string  :current_date
+      t.string  :current_date, :limit => 50
     end
 
     add_index :author_counters, :author_name
