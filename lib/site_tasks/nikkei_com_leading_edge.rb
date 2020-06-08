57151e19da011659e70e0d5716dd3547f22f106b
fanglongqing <fanglongqing@china-revival.com>
2020-06-08 22:16:20 +0800

    !

--- lib/site_tasks/nikkei_com_leading_edge.rb
+++ lib/site_tasks/nikkei_com_leading_edge.rb
@@ -8,8 +8,10 @@ class NikkeiComLeadingEdge
 
   def list(body)
     tasks = []
-      lk = "https://www.nikkei.com/technology/leading-edge/"
-
+    lk = "https://www.nikkei.com/technology/leading-edge/"
+    for i in 1..25
+      j = 20*i + 1
+      lk = "https://www.nikkei.com/technology/leading-edge/?bn=#{j}"
       str = RestClient.get(lk).body
       doc = Nokogiri::HTML(str)
       doc.search("h3.m-miM09_title").each do |item|
@@ -22,6 +24,7 @@ class NikkeiComLeadingEdge
         tasks << {mode:"item",body:URI.encode(body.to_json)}
         # tasks << {mode:"item",body:body}
       end
+    end
     return tasks
   end
 
