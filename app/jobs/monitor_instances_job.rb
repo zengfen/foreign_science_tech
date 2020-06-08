72062d06254af2e8983dd8794763a2409c368026
zengfen2 <zengfen@china-revival.com>
2020-06-08 17:28:29 +0800

    fix

--- app/jobs/monitor_instances_job.rb
+++ app/jobs/monitor_instances_job.rb
@@ -2,6 +2,7 @@ class MonitorInstancesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    puts "=====MonitorInstancesJob"
     spider_names = Spider.where(status:1).pluck(:spider_name)
     TSkJobInstance.where(spider_name:spider_names).each do |instance|
       puts "==instance_id====#{instance.id}="
--- app/jobs/t_sk_job_instances_job.rb
+++ app/jobs/t_sk_job_instances_job.rb
@@ -2,8 +2,9 @@ class TSkJobInstancesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts "==spider_name====#{args[0]["spider_name"]}==#{args[0]}=="
-    spider = Spider.where(spider_name:args[0]["spider_name"]).first
+    spider_name = args[0]["spider_name"]
+    puts "==spider_name====#{spider_name}===="
+    spider = Spider.where(spider_name:spider_name).first
     res,spider_task = spider.create_spider_task(SpiderTask::CycleTask)
     if res[:type] == "success"
       res2 = spider_task.start_task
