93a079ca24086e81ef8514116cc6d52de2e8a202
zengfen2 <zengfen@china-revival.com>
2020-06-11 20:43:13 +0800

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ end
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 8086 }
 
 # Specifies the `environment` that Puma will run in.
 #
