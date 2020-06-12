7508acc1f84d3e3748d241337db85468b80a2423
zengfen2 <zengfen@china-revival.com>
2020-06-12 11:42:40 +0800

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    ENV["API_ACESS_HOST"] = "127.0.0.1:8080"
     origins ENV["API_ACESS_HOST"]
     # origins "dev-okidb.china-revival.com"
     # origins "127.0.0.1:8080"
